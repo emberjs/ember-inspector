6b905d611011b8fba48b0d3bb89bf6466dfcf2fd
Teddy Zeenny <teddyzeenny@gmail.com>
2013-06-30 00:07:31 +0300

    Use Ember.computed.equal in MixinDetailController

--- extension_dist/panes/ember_extension.js
+++ extension_dist/panes/ember_extension.js
@@ -53,25 +53,23 @@ define("controllers/mixin_detail",
     var MixinDetailController = Ember.ObjectController.extend({
       needs: ['mixinDetails'],
 
-      isExpanded: function() {
-        return this.get('model.name') === 'Own Properties';
-      }.property('model.name'),
+      isExpanded: Ember.computed.equal('model.name', 'Own Properties'),
 
       digDeeper: function(property) {
         var objectId = this.get('controllers.mixinDetails.objectId');
-        this.get('port').send('digDeeper', { 
-          objectId: objectId, 
-          property: property.name 
+        this.get('port').send('digDeeper', {
+          objectId: objectId,
+          property: property.name
         });
       },
 
       calculate: function(property) {
         var objectId = this.get('controllers.mixinDetails.objectId');
         var mixinIndex = this.get('controllers.mixinDetails.mixins').indexOf(this.get('model'));
-        this.get('port').send('calculate', { 
-          objectId: objectId, 
-          property: property.name, 
-          mixinIndex: mixinIndex 
+        this.get('port').send('calculate', {
+          objectId: objectId,
+          property: property.name,
+          mixinIndex: mixinIndex
         });
       }
     });
