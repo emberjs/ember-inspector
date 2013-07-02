9b15a7a6c049126b59b1a1481d6c32bcadad5fbb
Teddy Zeenny <teddyzeenny@gmail.com>
2013-07-02 17:34:21 +0300

    Fix watch task options to use nospawn
    Fixes #19

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -1,11 +1,9 @@
 module.exports = {
   scripts: {
-    files: ['app/**', 'vendor/**', 'ember_debug/**', 'test/**/*', 'css/**/*'],
-    tasks: ['lock', 'build_test', 'unlock']
-    // TODO: Figure out why nospawn fails to detect file changes
-    // ,
-    // options: {
-    //   nospawn: true
-    // }
+    files: ['app/**/*', 'vendor/**/*', 'ember_debug/**/*', 'test/**/*', 'css/**/*'],
+    tasks: ['lock', 'build_test', 'unlock'],
+    options: {
+      nospawn: true
+    }
   }
 };
