455dd8f6e7f98157814176f5967838ab9fc9cd50
Jerry Nummi <jerry@edgecase.com>
2013-07-25 12:46:16 -0400

    Unfocused window styles

--- app/application.js
+++ app/application.js
@@ -4,7 +4,22 @@ import "resolver" as resolver;
 var App = Ember.Application.extend({
   modulePrefix: '',
   resolver: resolver,
-  Router: Router
+  Router: Router,
+  windowFocus: function(event) {
+    if (event.target.document.nodeType === Node.DOCUMENT_NODE) {
+      document.body.classList.remove('inactive');
+    }
+  },
+  windowBlur: function(event) {
+    if (event.target.document.nodeType === Node.DOCUMENT_NODE) {
+      document.body.classList.add('inactive');
+    }
+  },
+  init: function() {
+    this._super();
+    window.addEventListener('focus', this.windowFocus, false);
+    window.addEventListener('blur',  this.windowBlur,  false);
+  }
 });
 
 export = App;
