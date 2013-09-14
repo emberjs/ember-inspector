b53a32129e5b14a791977efeef84fcdb37bb850b
Luca Greco <luca.greco@alcacoop.it>
2013-09-15 01:36:20 +0200

    integrate Mozilla Addon SDK download and xpi generation
    - add grunt-mozilla-addon-sdk to npm devDependencies
    - configure new grunt tasks "mozilla-addon-sdk:download" and
      "mozilla-addon-sdk:xpi"

---
+++ tasks/options/mozilla-addon-sdk.js
@@ -0,0 +1,13 @@
+module.exports = {
+  download: {
+    options: {
+      revision: "release"
+    }
+  },
+  xpi: {
+    options: {
+      extension_dir: "ff_extension_dist",
+      dist_dir: "tmp/xpi"
+    }
+  }
+};
