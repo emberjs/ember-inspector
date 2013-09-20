3629df8f4789599c5969312481d477ed5068b6be
zoghal <saleh.souzanchi@gmail.com>
2013-09-21 04:25:24 +0430

    fix highlightedElment when page direction is rtl

--- ember_debug/view_debug.js
+++ ember_debug/view_debug.js
@@ -213,6 +213,8 @@ var ViewDebug = Ember.Object.extend(PortMixin, {
       backgroundColor: "rgba(255, 255, 255, 0.7)",
       border: "2px solid rgb(102, 102, 102)",
       padding: "0",
+      right: "auto",
+      direction: "ltr",
       boxSizing: "border-box",
       color: "rgb(51, 51, 255)",
       fontFamily: "Menlo, sans-serif",
