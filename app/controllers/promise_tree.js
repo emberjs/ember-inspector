1167cfc8d6bdeae28585fc9f920576d3a2afb51e
Teddy Zeenny <teddyzeenny@gmail.com>
2013-11-17 17:31:14 +0200

    Assemble promises and send them using PromiseDebug

--- app/controllers/promise_tree.js
+++ app/controllers/promise_tree.js
@@ -1,5 +1,5 @@
 var PromiseTreeController = Ember.ArrayController.extend({
-  children: Ember.computed.filterBy('model', 'parent', null)
+  children: Ember.computed.filterBy('model', 'parent', null),
 });
 
 export default PromiseTreeController;
