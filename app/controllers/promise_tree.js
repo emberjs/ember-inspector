4a80cfd6428b781e27f9c3d37ba1323ae9a14cb9
Teddy Zeenny <teddyzeenny@gmail.com>
2013-11-18 21:50:37 +0200

    Add promise filters and search

--- app/controllers/promise_tree.js
+++ app/controllers/promise_tree.js
@@ -1,8 +1,63 @@
 var PromiseTreeController = Ember.ArrayController.extend({
-  // children: Ember.computed.filterBy('filtered', 'pendingBranch', true),
-  children: Ember.computed.alias('filtered'),
 
-  filtered: Ember.computed.filterBy('model', 'parent', null)
+  children: Ember.computed.filter('model.@each.pendingBranch', function(item) {
+    if (!!item.get('parent')) { return false; } // this could be in separate filter but causing errors on re-render :/
+    var include = true;
+    if (this.get('filter') === 'pending') {
+      include = item.get('pendingBranch');
+    } else if (this.get('filter') === 'rejected') {
+      include = item.get('rejectedBranch');
+    } else if (this.get('filter') === 'fulfilled') {
+      include = item.get('fulfilledBranch');
+    }
+    if (!include) {
+      return false;
+    }
+    var search = this.get('effectiveSearch');
+    if (!Ember.isEmpty(search)) {
+      return item.matches(search);
+    }
+    return true;
+
+  }),
+
+
+  // filtered: Ember.computed.filterBy('model', 'parent', null),
+
+
+  filter: 'all',
+
+  noFilter: Ember.computed.equal('filter', 'all'),
+  isRejectedFilter: Ember.computed.equal('filter', 'rejected'),
+  isPendingFilter: Ember.computed.equal('filter', 'pending'),
+  isFulfilledFilter: Ember.computed.equal('filter', 'fulfilled'),
+
+  search: null,
+  effectiveSearch: null,
+
+  searchChanged: function() {
+    Ember.run.debounce(this, this.notifyChange, 500);
+
+  }.observes('search'),
+
+  notifyChange: function() {
+    var self = this;
+    this.set('effectiveSearch', this.get('search'));
+    Ember.run.next(function() {
+      self.notifyPropertyChange('children');
+    });
+  },
+
+  actions: {
+    setFilter: function(filter) {
+      var self = this;
+      this.set('filter', filter);
+      Ember.run.next(function() {
+        self.notifyPropertyChange('children');
+      });
+    }
+  }
 });
+
 
 export default PromiseTreeController;
--- app/models/promise.js
+++ app/models/promise.js
@@ -31,22 +31,58 @@ var Promise = Ember.Object.extend({
     return [];
   }.property(),
 
+  isPending: Ember.computed.not('isSettled'),
+
   pendingBranch: function() {
-    // if (!this.get('isSettled')) {
-    //   return true;
-    // }
-    // if (this.get('children')) {
-    //   for (var i = 0; i < this.get('children.length'); i++) {
-    //     if (this.get('children').objectAt(i).get('pendingBranch')) {
-    //       return true;
-    //     }
-    //   }
-    // }
-    return true;
-  }.property('isSettled', 'children.@each.pendingBranch')
+    return this.recursiveState('isPending', 'pendingBranch');
+  }.property('isPending', 'children.@each.pendingBranch'),
+
+  rejectedBranch: function() {
+    return this.recursiveState('isRejected', 'rejectedBranch');
+  }.property('isRejected', 'children.@each.rejectedBranch'),
+
+  fulfilledBranch: function() {
+    return this.recursiveState('isFulfilled', 'fulfilledBranch');
+  }.property('isFulfilled', 'children.@each.fulfilledBranch'),
+
+  recursiveState: function(prop, cp) {
+    if (this.get(prop)) {
+      return true;
+    }
+    if (this.get('children')) {
+      for (var i = 0; i < this.get('children.length'); i++) {
+        if (this.get('children').objectAt(i).get(cp)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+
+  searchIndex: function() {
+    var label = this.get('label') || '';
+    if (this.get('children')) {
+      for (var i = 0; i < this.get('children.length'); i++) {
+        label += ' ' + this.get('children').objectAt(i).get('searchIndex');
+      }
+    }
+    return label;
+  }.property('label', 'children.@each.label'),
+
+  matches: function(val) {
+    return !!this.get('searchIndex').toLowerCase().match(new RegExp('.*' + escapeRegExp(val.toLowerCase()) + '.*'));
+  },
+
+  matchesExactly: function(val) {
+    return !!this.get('label').toLowerCase().match(new RegExp('.*' + escapeRegExp(val.toLowerCase()) + '.*'));
+  }
 
 
 });
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
 
 
 export default Promise;
