ca1f614d0673646b425c908ee924cd259fbd7ef2
Luca Greco <luca.greco@alcacoop.it>
2014-02-15 19:51:54 +0100

    use postMessage to exchange messages between firefox addon and devtool panel code

--- app/adapters/firefox.js
+++ app/adapters/firefox.js
@@ -4,17 +4,22 @@ var FirefoxAdapter = BasicAdapter.extend({
 
   sendMessage: function(options) {
     options = options || {};
-    var event = document.createEvent("CustomEvent");
-    event.initCustomEvent("ember-extension-send", true, true, options);
-    document.documentElement.dispatchEvent(event);
+    window.parent.postMessage(options, "*");
   },
 
   _connect: function() {
     var self = this;
-    window.addEventListener("ember-extension-receive", function(evt) {
-      // We clone the object so that Ember prototype extensions
-      // are applied.
-      self._messageReceived(Ember.$.extend(true, {}, evt.detail));
+    // NOTE: chrome adapter sends a appId message on connect (not needed on firefox)
+    //this.sendMessage({ appId: "test" });
+    window.addEventListener("message", function(evt) {
+      // check if the event is originated by our privileged ember inspector code
+      if (evt.isTrusted) {
+        // We clone the object so that Ember prototype extensions
+        // are applied.
+        self._messageReceived(Ember.$.extend(true, {}, evt.data));
+      } else {
+        console.log("EMBER INSPECTOR: ignored post message", evt);
+      }
     }, false);
   }.on('init')
 });
