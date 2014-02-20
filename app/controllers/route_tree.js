stefanpenner/ember-extension

idiomatic improvements.

app/controllers/route_tree.js
@@ -1,5 +1,3 @@
-var RouteTreeController = Ember.ObjectController.extend({
+export default Ember.ObjectController.extend({
   currentRoute: null
 });
-
-export default RouteTreeController;

app/controllers/view_tree.js
@@ -1,4 +1,4 @@
-var ViewTreeController = Ember.ObjectController.extend({
+export default Ember.ObjectController.extend({
   pinnedObjectId: null,
   inspectingViews: false,
 
@@ -27,5 +27,3 @@ var ViewTreeController = Ember.ObjectController.extend({
     }
   }
 });
-
-export default ViewTreeController;

emberjs/ember-inspector

app/controllers/route_tree.js
@@ -1,7 +1,5 @@
-var RouteTreeController = Ember.ArrayController.extend({
+export default Ember.ArrayController.extend({
   needs: ['application'],
   itemController: 'routeItem',
   currentRoute: null
 });
-
-export default RouteTreeController;

app/controllers/view_tree.js
@@ -1,4 +1,4 @@
-var ViewTreeController = Ember.ArrayController.extend({
+export default Ember.ArrayController.extend({
   needs: ['application'],
   itemController: 'viewItem',
   pinnedObjectId: null,
@@ -37,5 +37,3 @@ var ViewTreeController = Ember.ArrayController.extend({
     }
   }
 });
-
-export default ViewTreeController;