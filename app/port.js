54c7fc6e6c1129c31409e91bf503bd296e2ce693
Teddy Zeenny <teddyzeenny@gmail.com>
2014-02-23 21:15:30 +0200

    Added multiple iframe support

--- app/port.js
+++ app/port.js
@@ -1,14 +1,35 @@
 export default Ember.Object.extend(Ember.Evented, {
+  applicationId: undefined,
+
+  detectedApplications: function() {
+    return [];
+  }.property(),
+
   init: function() {
-    var self = this;
+    var detectedApplications = this.get('detectedApplications');
     this.get('adapter').onMessageReceived(function(message) {
-      self.trigger(message.type, message);
-    });
+      if (!message.applicationId) {
+        return;
+      }
+      if (!this.get('applicationId')) {
+        this.set('applicationId', message.applicationId);
+      }
+      // save list of application ids
+      if (detectedApplications.indexOf(message.applicationId) === -1) {
+        detectedApplications.pushObject(message.applicationId);
+      }
+
+      var applicationId = this.get('applicationId');
+      if (applicationId === message.applicationId) {
+        this.trigger(message.type, message, applicationId);
+      }
+    }.bind(this));
   },
   send: function(type, message) {
     message = message || {};
     message.type = type;
     message.from = 'devtools';
+    message.applicationId = this.get('applicationId');
     this.get('adapter').sendMessage(message);
   }
 });
--- ember_debug/port.js
+++ ember_debug/port.js
@@ -1,17 +1,27 @@
-var Port = Ember.Object.extend(Ember.Evented, {
-  adapter: Ember.computed.alias('namespace.adapter'),
+var oneWay = Ember.computed.oneWay;
+var guidFor = Ember.guidFor;
+
+export default Ember.Object.extend(Ember.Evented, {
+  adapter: oneWay('namespace.adapter').readOnly(),
+
+  application: oneWay('namespace.application').readOnly(),
+
+  uniqueId: Ember.computed(function() {
+    return guidFor(this.get('application')) + '__' + window.location.href + '__' + Date.now();
+  }).property(),
 
   init: function() {
     var self = this;
     this.get('adapter').onMessageReceived(function(message) {
-      self.trigger(message.type, message);
+      if(self.get('uniqueId') === message.applicationId || !message.applicationId) {
+        self.trigger(message.type, message);
+      }
     });
   },
   send: function(messageType, options) {
     options.type = messageType;
     options.from = 'inspectedWindow';
+    options.applicationId = this.get('uniqueId');
     this.get('adapter').sendMessage(options);
   }
 });
-
-export default Port;
