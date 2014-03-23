039a842a58e2661c37396a2c1a3e902a8c72e089
Teddy Zeenny <teddyzeenny@gmail.com>
2014-03-23 15:17:08 +0200

    Stop cleaning dist_chrome on every build
    Commit 497c568cef77db519a3b1d423a44003a23ac267b started to clean
    dist_chrome before every build so we can detect missing vendor files
    instead of them staying permanently on a developer's machine, while
    failing on new clones.
    
    The problem is that it is not possible to block the extension refresh
    during build, which is causing very annoying errors.
    
    I kept cleaning firefox_dist so we can at least detect missing files
    when testing Firefox.

--- tasks/options/clean.js
+++ tasks/options/clean.js
@@ -1,8 +1,12 @@
 module.exports = [
   'tmp',
-  'dist_chrome/images',
-  'dist_chrome/ember_debug',
-  'dist_chrome/vendor',
+  // The below is commented because we are unable to block while building
+  // which would result in errors if we refresh quickly
+
+  // 'dist_chrome/images',
+  // 'dist_chrome/ember_debug',
+  // 'dist_chrome/vendor',
+
   'dist_firefox/data/images',
   'dist_firefox/data/ember_debug',
   'dist_firefox/data/vendor'
