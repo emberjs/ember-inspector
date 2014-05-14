69757b5236ef0c73fd56a6d3f28fcff126d5236e
Teddy Zeenny <teddyzeenny@gmail.com>
2014-05-14 19:35:27 +0300

    Fix time zone issue causing render tree test to fail

--- test/ember_extension/render_tree_test.js
+++ test/ember_extension/render_tree_test.js
@@ -5,18 +5,18 @@ function generateProfiles() {
     {
       name: 'First View Rendering',
       duration: 476.87,
-      timestamp: 1399716982715,
+      timestamp: (new Date(2014, 5, 1, 13, 16, 22, 715)).getTime(),
       children: [{
         name: 'Child view',
         duration: 0.36,
-        timestamp: 1399716982581,
+        timestamp: (new Date(2014, 5, 1, 13, 16, 22, 581)).getTime(),
         children: []
       }]
     },
     {
       name: "Second View Rendering",
       duration: 10,
-      timestamp: 1399716982759,
+      timestamp: (new Date(2014, 5, 1, 13, 16, 22, 759)).getTime(),
       children: []
     }
   ];
