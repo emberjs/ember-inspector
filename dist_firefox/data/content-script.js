8d7a19c8b7abc2faae140d2129b4faa5f5841e4e
Luca Greco <luca.greco@alcacoop.it>
2014-07-29 12:00:54 +0000

    FEATURE: iframes on Firefox addon
    This commit introduces a complete refactoring/cleanup of the Firefox
    version, it supports Ember apps embedded into iframes on Firefox
    (Ember libraries detection and Devtool panel connection) and it has
    an optional tomster icon in the locationbar.
    
    The tomster icon can be enabled/disabled from the addon preferences,
    and it will be visible when an Ember app is detected in the tab or
    in an iframe inside the tab).
    
    Requirements:
    - Firefox >= 30.0 (needed for the "evalInWindow" method used in content-script)
    
    Tested on:
    - http://jsfiddle.net/9zHj9/1/
    
    to be merged into #182

--- dist_firefox/data/content-script.js
+++ dist_firefox/data/content-script.js
@@ -1,5 +1,87 @@
+console.debug("LOADING CONTENT SCRIPT", window.location && window.location.href);
+
+init();
+
+function init() {
+  window.addEventListener("message", onEmberVersion, true);
+  document.addEventListener("ember-debug-send", onEmberDebugEvent, false);
+  self.port.on("emberDevTool", onEmberInspectorMessage);
+  self.port.on("injectEmberDebug", onInjectEmberDebug);
+
+  // NOTE: needed to cleanup on legacy Firefox versions
+  self.on("detach", onCleanupListeners);
+  // NOTE: needed to cleanup on Firefox >= 29
+  self.port.on("detach", onCleanupListeners);
+
+  self.port.emit("resetEmberIcon", { type: "resetEmberIcon" });
+  injectInPageScript();
+  sendIframes();
+
+  // let ember-debug know that content script has executed
+  document.documentElement.dataset.emberExtension = 1;
+
+  // Allow older versions of Ember (< 1.4) to detect the extension.
+  if (document.body) {
+    document.body.dataset.emberExtension = 1;
+  }
+}
+
+function onCleanupListeners() {
+  console.debug("DETACHING CONTENT SCRIPT", window.location && window.location.href);
+  try {
+    window.removeEventListener("message", onEmberVersion, true);
+  } catch(e) {}
+  try {
+    document.removeEventListener("ember-debug-send", onEmberDebugEvent, false);
+  } catch(e) {}
+  try {
+    self.port.removeListener("emberDevTool", onEmberInspectorMessage);
+  } catch(e) {}
+  try {
+    self.port.removeListener("injectEmberDebug", onInjectEmberDebug);
+  } catch(e) {}
+}
+
+function onEmberVersion(message) {
+  var data = message.wrappedJSObject.data;
+  if (data && data.type === "emberVersion") {
+    self.port.emit("emberVersion", data);
+  }
+}
+
+function onInjectEmberDebug() {
+  //console.debug("ON INJECT EMBER DEBUG ON:", window.location.href);
+  // NOTE: evalInWindow needs Firefox >= 30.0
+  evalInWindow(self.options.emberDebugScript, unsafeWindow);
+}
+
+function injectInPageScript() {
+  if (window.document.readyState == "complete") {
+    // inject JS into the page to check for an app on domready
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = self.options.inPageScriptURL;
+    if (document.body) document.body.appendChild(script);
+  } else {
+    setTimeout(injectInPageScript, 100);
+  }
+}
+
+function sendIframes() {
+  var iframes = document.getElementsByTagName('iframe');
+  var urls = [];
+  for (var i = 0, l = iframes.length; i < l; i ++) {
+    urls.push(iframes[i].src);
+  }
+
+  // FIXME
+  setTimeout(function() {
+    self.port.emit("emberDebug", { type: 'iframes', urls: urls});
+  }, 500);
+}
+
 function onEmberInspectorMessage(message) {
-  console.debug("content-script: ember debug receive", message);
+  //console.debug("content-script: ember debug receive", message);
 
   let event = document.createEvent("CustomEvent");
 
@@ -17,16 +99,7 @@ function onEmberInspectorMessage(message) {
 }
 
 function onEmberDebugEvent(event) {
-  console.debug("content-script: ember debug send", event.detail);
+  //console.debug("content-script: ember debug send", event.detail);
 
-  self.port.emit("message", event.detail);
+  self.port.emit("emberDebug", event.detail);
 }
-
-console.log("EXECUTING EMBER DEBUG BRIDGE on: ", document.defaultView.location.toString());
-
-document.addEventListener("ember-debug-send", onEmberDebugEvent, false);
-
-self.port.on("message", onEmberInspectorMessage);
-
-// let ember-debug know that content script has executed
-document.documentElement.dataset.emberExtension = 1;
--- tasks/options/mozilla-addon-sdk.js
+++ tasks/options/mozilla-addon-sdk.js
@@ -1,7 +1,8 @@
 module.exports = {
   "1_15": {
     options: {
-      revision: "1.15"
+      revision: "1.15",
+      dest_dir: ".mozilla-addon-sdk"
     }
   }
 };
