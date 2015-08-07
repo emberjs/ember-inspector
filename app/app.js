aba6a2a3d2d306efc8120d1db4a59f49ae401eee
Okke Timm <okke@timm.sh>
2015-08-07 15:06:59 +0200

    Remove the `const version` string
    The app now uses the version string provided by ember-cli-app-version.

--- app/app.js
+++ app/app.js
@@ -10,8 +10,6 @@ import msToTime from "ember-inspector/helpers/ms-to-time";
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-const version = '1.9.0';
-
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
