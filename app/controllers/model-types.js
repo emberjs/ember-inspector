ee759f51fed8f8155a845303f0530dab7b53bc31
Vasil Vanchuk <v.vanchuk@tut.by>
2015-08-28 12:20:39 +0300

    Add 'Hide empty models' filter

--- app/controllers/model-types.js
+++ app/controllers/model-types.js
@@ -1,9 +1,22 @@
 import Ember from "ember";
-const { Controller, computed: { sort } } = Ember;
+const { Controller, computed: { sort, filter }, get } = Ember;
 
 export default Controller.extend({
   navWidth: 180,
   sortProperties: ['name'],
+  options: {
+    hideEmptyModelTypes: false
+  },
 
-  sorted: sort('model', 'sortProperties')
+  sorted: sort('filtered', 'sortProperties'),
+
+  filtered: filter('model', function(typeItem) {
+    let hideEmptyModels = get(this, 'options.hideEmptyModelTypes');
+
+    if (hideEmptyModels) {
+      return !!get(typeItem, 'count');
+    } else {
+      return true;
+    }
+  }).property('model', 'options.hideEmptyModelTypes')
 });
