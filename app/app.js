b50f37798a8187bbf39d5bad66c029633f5bf8bd
Raido Kuli <raido357@gmail.com>
2016-06-06 19:08:45 +0300

    WIP Initial commit on Ember v1.12 > v1.13 upgrade

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ const version = '{{EMBER_INSPECTOR_VERSION}}';
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 
