f297ff88f33991f4e41d06296a7215fd092ee567
Raido Kuli <raido357@gmail.com>
2016-06-06 13:07:44 +0300

    Workaround for Ember v1.10.0 to v1.12.0 update Container/Registry reset.
    See docblock for explanation why this is required.

--- app/app.js
+++ app/app.js
@@ -61,8 +61,33 @@ App.initializer({
   }
 });
 
-
 loadInitializers(App, config.modulePrefix);
 
+/**
+ * This is here a "temporary" workaround for Container reset that Ember v1.10 provided "out of the box"
+ *
+ * Problem: Ember v1.11.0 introduced ApplicationInstance concept and with this Application.reset()
+ * method changed. In v1.10.0 reset() always created new Container() and thus everything in Container was reset.
+ * Since v1.11.0 it no longer does that but Inspector depends on that behavior, because inspector is
+ * able to instrument multiple applications, typically embedded Ember apps in iframes.
+ *
+ * See "controllers/iframes.js" for app.reset() call.
+ *
+ * Without this here, this is no longer possible and Inspector crashes, because
+ * it cannot re-register required injections, like adapter:main when new application is selected for inspection.
+ * Also it does not help us to protect initializers in the app with registry.has('my:thing')
+ * because we cannot protect against third party initializers.
+ *
+ * Therefore current solution is to reopen App and call buildRegistry() on reset().
+ *
+ * // TODO: Improve support for multiple app instrumentation, in a way that app.reset() call is not required
+ */
+
+App.reopen({
+  reset() {
+    this.buildRegistry();
+    this._super(...arguments);
+  }
+});
 
 export default App;
