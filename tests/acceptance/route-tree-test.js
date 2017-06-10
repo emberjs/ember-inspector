b9809c23f0563e98577dfa1f28f26cd873e9ed1a
cibernox <miguel.camba@gmail.com>
2017-06-10 21:42:02 +0200

    Remove last usage of jQuery in tests

--- tests/acceptance/route-tree-test.js
+++ tests/acceptance/route-tree-test.js
@@ -21,8 +21,26 @@ module('Route Tree Tab', {
   }
 });
 
-function deepAssign(...objects) {
-  return Ember.$.extend(true, ...objects);
+export function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+export function deepAssign(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        deepAssign(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return deepAssign(target, ...sources);
 }
 
 function routeValue(name, props) {
