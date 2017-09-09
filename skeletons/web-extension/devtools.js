d6bb6350ddd7bf9d1afd531b08fd116f90d8b707
Teddy Zeenny <teddyzeenny@gmail.com>
2017-09-09 15:14:43 +0300

    Update skeletons to use WebExtensions

--- skeletons/web-extension/devtools.js
+++ skeletons/web-extension/devtools.js
@@ -6,4 +6,4 @@
  */
 var panelWindow, injectedPanel = false, injectedPage = false, panelVisible = false, savedStack = [];
 
-chrome.devtools.panels.create("Ember{{env}}", "images/ember-icon-final.png", "{{PANE_ROOT}}/index.html");
+chrome.devtools.panels.create("Ember{{env}}", "{{PANE_ROOT}}/assets/images/icon16.png", "{{PANE_ROOT}}/index.html");
