0c2a8147776a4d7ce53ed5be09a24ddd92b7f3c4
Eli Flanagan <efx@users.noreply.github.com>
2017-12-28 10:39:50 -0500

    Preserve model type hiding state (#742)

--- app/controllers/model-types.js
+++ app/controllers/model-types.js
@@ -1,21 +1,33 @@
-import Ember from "ember";
+import Ember from 'ember';
+import LocalStorageService from 'ember-inspector/services/storage/local';
 const { Controller, computed, get, inject } = Ember;
 const { sort } = computed;
-const { controller } = inject;
+const { controller, service } = inject;
 
 export default Controller.extend({
   application: controller(),
   navWidth: 180,
   sortProperties: ['name'],
-  options: {
-    hideEmptyModelTypes: false
-  },
+  storage: service(`storage/${LocalStorageService.SUPPORTED ? 'local' : 'memory'}`),
+  hideEmptyModelTypes: computed({
+    get() {
+      return !!this.get('storage').getItem('are-model-types-hidden');
+    },
+    set(key, value) {
+      if (!value) {
+        this.get('storage').removeItem('are-model-types-hidden');
+      } else {
+        this.get('storage').setItem('are-model-types-hidden', value);
+      }
+      return value;
+    }
+  }),
 
   sorted: sort('filtered', 'sortProperties'),
 
-  filtered: computed('model.@each.count', 'options.hideEmptyModelTypes', function() {
+  filtered: computed('model.@each.count', 'hideEmptyModelTypes', function() {
     return this.get('model').filter(item => {
-      let hideEmptyModels = get(this, 'options.hideEmptyModelTypes');
+      let hideEmptyModels = get(this, 'hideEmptyModelTypes');
 
       if (hideEmptyModels) {
         return !!get(item, 'count');
