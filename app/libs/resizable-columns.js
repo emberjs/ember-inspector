824e95333a2536972d28c429527a66048e8ef8bb
Teddy Zeenny <teddyzeenny@gmail.com>
2018-05-31 18:24:17 +0300

    Fix toggling and resizing table columns (#815)
    * Fix toggling table columns
    
    * Fix column resizing

--- app/libs/resizable-columns.js
+++ app/libs/resizable-columns.js
@@ -208,11 +208,11 @@ export default class {
   buildColumns() {
     this.buildColumnVisibility();
     let totalWidth = 0;
-    let columns = this._columnVisibility.filterBy('visible')
-      .map(({ id, name }) => {
-        let width = this.getColumnWidth(id);
+    let columns = this._columnVisibility
+      .map(({ id, name, visible }) => {
+        let width = visible ? this.getColumnWidth(id) : 0;
         totalWidth += width;
-        return { width, id, name };
+        return { width, id, name, visible };
       });
     // Fix percentage precision errors. If we only add it to the last column
     // the last column will slowly increase in size every time we visit this list.
@@ -220,7 +220,7 @@ export default class {
     if (columns.length > 0) {
       let diff = this.tableWidth - totalWidth;
       while (diff > 0) {
-        columns.sortBy('width').forEach(column => {
+        columns.filterBy('visible').sortBy('width').forEach(column => {
           if (diff > 0) {
             column.width++;
             diff--;
@@ -284,7 +284,7 @@ export default class {
   processColumns() {
     let columns = this._columns;
     let prevLeft, prevWidth;
-    columns = columns.map(({ id, name, width }, index) => {
+    columns = columns.map(({ id, name, width, visible }, index) => {
       let last = this._columns[this._columns.length - 1];
       let left = 0;
       if (index > 0) {
@@ -293,7 +293,7 @@ export default class {
       let maxWidth = width + last.width - this.minWidth;
       prevLeft = left;
       prevWidth = width;
-      return { id, name, width, left, maxWidth };
+      return { id, name, width, left, maxWidth, visible };
     });
     this.saveVisibility();
     this.saveWidths();
