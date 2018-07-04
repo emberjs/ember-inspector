6db429cf39dad9b04b97fdc211c39cf46ea9e699
John Costanzo <jrock2004@gmail.com>
2018-07-03 22:36:47 -0400

    Adding security policy to see if this fixes #819

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+    contentSecurityPolicy: {
+      'default-src': ["'none'"],
+      'script-src': ["'self'"],
+      'font-src': ["'self'"],
+      'connect-src': ["'self'"],
+      'img-src': ["'self'"],
+      'style-src': ["'self'"],
+      'media-src': ["'self'"]
+    },
+    contentSecurityPolicyMeta: true
   };
 
   if (environment === 'development') {
