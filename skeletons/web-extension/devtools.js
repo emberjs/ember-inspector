68ed6e7bb629d9db84401878428f5046dcd99a94
Dipil Singh Saud <dipil.saud@gmail.com>
2018-10-11 15:22:27 +0545

    Update the component icon from the tomster to ember's rounded-square ‘e’ icon.

--- skeletons/web-extension/devtools.js
+++ skeletons/web-extension/devtools.js
@@ -6,4 +6,4 @@
  */
 var panelWindow, injectedPanel = false, injectedPage = false, panelVisible = false, savedStack = [];
 
-chrome.devtools.panels.create("Ember{{env}}", "{{PANE_ROOT}}/assets/images/icon16.png", "{{PANE_ROOT}}/index.html");
+chrome.devtools.panels.create("Ember{{env}}", "{{PANE_ROOT}}/assets/svg/ember-icon.svg", "{{PANE_ROOT}}/index.html");
