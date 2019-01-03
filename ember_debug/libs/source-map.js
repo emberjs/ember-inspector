5284526137651c25ee3ba40fc8e329b1d8b8afe8
Mark Oleson <fusion2004@gmail.com>
2019-01-03 18:25:20 -0500

    When forming the URL to retrieve a source map, if it is already an absolute URL then use it as is (emberjs#908)

--- ember_debug/libs/source-map.js
+++ ember_debug/libs/source-map.js
@@ -92,7 +92,14 @@ function retrieveSourceMap(source) {
 }
 
 function relativeToAbsolute(file, url) {
-  if (!file) { return url; }
+  // Regex from https://stackoverflow.com/a/19709846
+  // This will match the most common prefixes we care about: "http://", "https://", "//"
+  let absoluteUrlRegex = new RegExp('^(?:[a-z]+:)?//', 'i');
+
+  // If we don't have a file URL or the sourcemap URL is absolute, then return the sourcemap URL.
+  if (!file || absoluteUrlRegex.test(url)) { return url; }
+
+  // Otherwise, find the sourcemap URL relative to the original file.
   let dir = file.split('/');
   dir.pop();
   dir.push(url);
