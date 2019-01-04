24a7f29abf46441d944b81d80189b86bbfa170b9
Jerry Nummi <nummi@users.noreply.github.com>
2019-01-03 17:45:13 -0800

    Use ember-table for Libraries tab (#907)

---
+++ app/controllers/libraries.js
@@ -0,0 +1,11 @@
+import { computed } from '@ember/object';
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  rows: computed('model.[]', function() {
+    return this.get('model.[]').map(({ name, version }) => ({
+      library: name,
+      version
+    }));
+  }),
+});
