18da7a5474892d3d695b83c1a2cc953b99e794ed
benjaminJ <kamikillerto@gmail.com>
2019-04-23 21:34:06 +0200

    build: Rewrite the grunt compress tasks in gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+const gulp = require("gulp");
+const zip = require("gulp-zip");
+const pkg = require('./package.json');
+let versionedPane = `panes-${pkg.emberVersionsSupported[0].replace(/\./g, '-')}`;
+
+function compress(baseSrc, archiveName) {
+  return gulp.src(`${baseSrc}/**/*`)
+    .pipe(zip(archiveName))
+    .pipe(gulp.dest("dist"));
+}
+
+exports["compress:chrome"] = () => compress("dist/chrome", "chrome/ember-inspector.zip");
+exports["compress:firefox"] = () => compress("dist/firefox", "firefox/ember-inspector.zip");
+exports["compress:chrome-pane"] = () => compress(`dist/chrome/${versionedPane}`, "chrome-pane.zip");
+exports["compress:firefox-pane"] = () => compress(`dist/firefox/${versionedPane}`, "firefox-pane.zip");
+exports["compress:bookmarklet-pane"] = () => compress(`dist/bookmarklet/${versionedPane}`, "bookmarklet-pane.zip");
