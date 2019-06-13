875dd2f0c5610c71284882e7c00583826665c159
Robert Wagner <rwwagner90@gmail.com>
2019-06-13 00:11:23 -0400

    Enable 3 template lint rules and fix templates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,9 +6,6 @@ module.exports = {
     'link-rel-noopener': false,
     'no-inline-styles': false,
     'no-invalid-interactive': false,
-    'no-outlet-outside-routes': false,
-    'no-unbound': false,
-    'simple-unless': false,
     'table-groups': false
   }
 };
