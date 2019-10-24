60c1e36ef37e5da66f5434ff1e5cde9020a90b08
patricklx <patricklx@users.noreply.github.com>
2019-10-24 12:07:34 +0200

    fix

--- app/components/object-inspector/sort-properties.js
+++ app/components/object-inspector/sort-properties.js
@@ -20,7 +20,7 @@ export default Component.extend({
   sortedProperties: sort('props', 'sortProperties'),
 
   props: map('properties', function (p) {
-    set(p, 'isFunction', p.value.type.includes('function');
+    set(p, 'isFunction', p.value.type.includes('function'));
     if (p.name == parseInt(p.name)) {
       set(p, 'name', parseInt(p.name));
     }
