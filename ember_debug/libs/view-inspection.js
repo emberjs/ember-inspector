08948f4d66b46de78afe6960f63e3b540a807a81
Godfrey Chan <godfreykfc@gmail.com>
2019-11-19 00:21:45 -0800

    [WIP] view inspecting

---
+++ ember_debug/libs/view-inspection.js
@@ -0,0 +1,446 @@
+const Ember = window.Ember;
+const { classify } = Ember.String;
+
+function makeHighlight(id) {
+  return `<div id="ember-inspector-highlight-${id}" role="presentation"></div>`;
+}
+
+function makeTooltip(id) {
+  let prefix = 'ember-inspector-tooltip';
+
+  return `
+    <div id="${prefix}-${id}">
+      <span class="${prefix}-header">
+        <span class="${prefix}-title"></span>
+        <span class="${prefix}-category"></span>
+      </span>
+      <table class="${prefix}-details">
+        <tbody>
+        </tbody>
+      </table>
+      <div class="${prefix}-arrow" role="presentation"></div>
+    </div>
+  `;
+}
+
+function makeStylesheet(id) {
+  let prefix = 'ember-inspector';
+
+  return `
+    #${prefix}-highlight-${id} {
+      display: none;
+      position: absolute;
+      padding: 0px;
+      margin: 0px;
+      z-index: 10000;
+      pointer-events: none;
+      /* https://github.com/ChromeDevTools/devtools-frontend/blob/b336f0440a8fb539352ac223ef466c3475618cf1/front_end/common/Color.js#L904 */
+      background: rgba(111, 168, 220, .66);
+    }
+
+    #${prefix}-tooltip-${id} {
+      display: none;
+      position: absolute;
+      box-sizing: border-box;
+      padding: 4px 8px;
+      margin: 8px 0px;
+      z-index: 10000;
+      font-family: sans-serif;
+      font-size: 12px;
+      background: white;
+      box-shadow: 0px 2px 8px 0px rgba(0,0,0,0.25);
+      border-radius: 3px;
+      pointer-events: none;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-header {
+      display: block;
+      margin: 4px 0px;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-title {
+      font-weight: bold;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-token-tag,
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-token-namespace {
+      /* https://github.com/ChromeDevTools/devtools-frontend/blob/103326238685ac582d3bf2a02f1627a80e3fce5f/front_end/ui/inspectorSyntaxHighlight.css#L69-L71 */
+      color: rgb(168, 148, 166);
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-token-name {
+      /* https://github.com/ChromeDevTools/devtools-frontend/blob/103326238685ac582d3bf2a02f1627a80e3fce5f/front_end/ui/inspectorSyntaxHighlight.css#L60 */
+      color: rgb(136, 18, 128);
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-token-id {
+      /* https://github.com/ChromeDevTools/devtools-frontend/blob/103326238685ac582d3bf2a02f1627a80e3fce5f/front_end/ui/inspectorSyntaxHighlight.css#L109-L113 */
+      color: rgb(26, 26, 166);
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-details {
+      table-layout: auto;
+      width: auto;
+      margin: 0px;
+      padding: 0px;
+      border: none;
+      border-spacing: 0;
+      border-collapse: collapse;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-details th {
+      display: block;
+      margin: 4px 8px 4px 0px;
+      padding: 0px;
+      border: none;
+      white-space: nowrap;
+      font-weight: normal;
+      text-align: left;
+      color: #666;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-details td {
+      white-space: nowrap;
+      margin: 0px;
+      padding: 0px;
+      border: none;
+      text-align: right;
+      color: #000;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-arrow {
+      position: absolute;
+      width: 40px;
+      height: 20px;
+      left: 0px;
+      bottom: -20px;
+      overflow: hidden;
+    }
+
+    #${prefix}-tooltip-${id} .${prefix}-tooltip-arrow::after {
+      content: "";
+      position: absolute;
+      width: 0px;
+      height: 0px;
+      top: 0px;
+      left: 8px;
+      box-sizing: border-box;
+      border: 6px solid white;
+      border-color: transparent transparent white white;
+      transform-origin: 0 0;
+      transform: rotate(-45deg);
+      box-shadow: 0px 2px 8px 0px rgba(0,0,0,0.25);
+      pointer-events: none;
+    }
+  `;
+}
+
+export default class ViewInspection {
+  constructor({ renderTree, objectInspector }) {
+    this.renderTree = renderTree;
+    this.objectInspector = objectInspector;
+
+    this.id = (Math.random() * 100000000).toFixed(0);
+    this.isInspecting = false;
+    this.lastTarget = null;
+    this.lastMatch = null;
+
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onClick = this.onClick.bind(this);
+
+    this.setup();
+  }
+
+  setup() {
+    let { id } = this;
+    this.highlight = this._insertHTML(makeHighlight(id));
+    this.tooltip = this._insertHTML(makeTooltip(id));
+    this._insertStylesheet(makeStylesheet(id));
+  }
+
+  start() {
+    this.isInspecting = true;
+    this.lastTarget = null;
+    this.lastMatch = null;
+
+    document.body.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  stop() {
+    this.hide();
+
+    this.isInspecting = false;
+    this.lastTarget = null;
+    this.lastMatchId = null;
+
+    document.body.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMouseMove({ target }) {
+    this.inspectNearest(target, false);
+  }
+
+  onClick({ target }) {
+
+  }
+
+  inspectNearest(target, pin = true) {
+    let { isInspecting, lastTarget, lastMatchId } = this;
+
+    if (isInspecting && target === lastTarget) {
+      return this.renderTree.find(lastMatchId);
+    }
+
+    let match = this.renderTree.findNearest(target, lastMatchId);
+
+    if (match) {
+      this.show(match.id, pin);
+    } else {
+      this.hide();
+    }
+
+    if (isInspecting) {
+      this.lastTarget = target;
+      this.lastMatchId = match && match.id;
+    }
+
+    return match;
+  }
+
+  show(id, pin = true) {
+    let node = this.renderTree.find(id);
+    let rect = this.renderTree.getBoundingClientRect(id);
+
+    if (node && rect) {
+      this._showHighlight(node, rect);
+      this._showTooltip(node, rect);
+    } else {
+      this.hide();
+    }
+  }
+
+  hide() {
+    this._hideHighlight();
+    this._hideTooltip();
+  }
+
+  _showHighlight(_node, rect) {
+    let { style } = this.highlight;
+    let { top, left, width, height } = rect;
+    let { scrollX, scrollY } = window;
+
+    style.display = 'block';
+    style.top = `${top + scrollY}px`;
+    style.left = `${left + scrollX}px`;
+    style.width = `${width}px`;
+    style.height = `${height}px`;
+    style.pointerEvents = 'none';
+  }
+
+  _hideHighlight() {
+    this.highlight.style.display = 'none';
+  }
+
+  _showTooltip(node, rect) {
+    this._renderTooltipTitle(node);
+    this._renderTooltipCategory(node);
+    this._renderTooltipDetails(node);
+
+    let { style } = this.tooltip;
+    let { top, left } = rect;
+    let { scrollX, scrollY } = window;
+
+    style.display = 'block';
+    style.top = 'auto';
+    style.bottom = 'auto';
+    style.left = 'auto';
+    style.right = 'auto';
+    style.pointerEvents = 'none';
+
+    let { height } = this.tooltip.getBoundingClientRect();
+
+    style.top = `${top + scrollY - height - 16}px`;
+    style.left = `${left + scrollX}px`;
+  }
+
+  _hideTooltip() {
+    this.tooltip.style.display = 'none';
+  }
+
+  _renderTooltipTitle(node) {
+    let title = this.tooltip.querySelector('.ember-inspector-tooltip-title');
+
+    title.innerHTML = '';
+
+    if (node.type === 'component') {
+      this._renderTokens(title, this._tokenizeComponentNode(node));
+    } else if (node.type === 'outlet') {
+      this._renderTokens(title, [['tag', '{{'], ['name', 'outlet'], ['id', node.name], ['tag', '}}']]);
+    } else if (node.type === 'engine') {
+      this._renderTokens(title, [['tag', '{{'], ['name', 'mount'], ['id', node.name], ['tag', '}}']]);
+    } else {
+      title.innerText = node.name;
+    }
+  }
+
+  _renderTooltipCategory(node) {
+    let category = this.tooltip.querySelector('.ember-inspector-tooltip-category');
+
+    switch (node.type) {
+      case 'component':
+      case 'outlet':
+      case 'engine':
+        category.innerHTML = '';
+        break;
+
+      case 'route-template':
+        category.innerText = 'route';
+        break;
+    }
+  }
+
+  _renderTooltipDetails(node) {
+    let tbody = this.tooltip.querySelector('.ember-inspector-tooltip-details tbody');
+
+    tbody.innerHTML = '';
+
+    if (node.template) {
+      this._renderTooltipDetail(tbody, 'Template', node.template);
+    }
+
+    if (node.instance) {
+      if (node.type === 'route-template') {
+        this._renderTooltipDetail(tbody, 'Controller', this._tokenizeItem(node.instance));
+      } else {
+        this._renderTooltipDetail(tbody, 'Instance', this._tokenizeItem(node.instance));
+      }
+    }
+  }
+
+  _renderTooltipDetail(tbody, key, value) {
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
+    let td = document.createElement('td');
+
+    th.innerText = key;
+
+    if (Array.isArray(value)) {
+      this._renderTokens(td, value);
+    } else {
+      td.innerText = value;
+    }
+
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+  }
+
+  _renderTokens(parent, tokens) {
+    for(let [type, value] of tokens) {
+      let span = document.createElement('span');
+      span.innerText = value;
+      span.setAttribute('class', `ember-inspector-tooltip-token-${type}`);
+      parent.appendChild(span);
+    }
+  }
+
+  _tokenizeComponentNode(node) {
+    let useAngleBracket = node.args.positional.length === 0;
+    let parts = node.name.split('/');
+
+    if (useAngleBracket) {
+      parts = parts.map(classify);
+    }
+
+    let name = parts.pop();
+    let namespace = parts;
+
+    let tokens = [];
+
+    if (useAngleBracket) {
+      tokens.push(['tag', '<']);
+    } else {
+      tokens.push(['tag', '{{']);
+    }
+
+    while (namespace.length > 0) {
+      tokens.push(['namespace', namespace.shift()]);
+      tokens.push(['tag', '::']);
+    }
+
+    tokens.push(['name', name]);
+
+    if (useAngleBracket) {
+      tokens.push(['tag', '>']);
+    } else {
+      tokens.push(['tag', '}}']);
+    }
+
+    return tokens;
+  }
+
+  _tokenizeItem(item) {
+    switch (typeof item) {
+      case 'string':
+      case 'number':
+      case 'bigint':
+      case 'boolean':
+      case 'undefined':
+        return [['id', `${item}`]];
+    }
+
+    if (item === null) {
+      return [['id', 'null']];
+    }
+
+    return this._tokenizeObject(item);
+  }
+
+  _tokenizeObject(item) {
+    let object = this.objectInspector.sentObjects[item.id];
+    let stringified;
+
+    try {
+      stringified = String(object);
+    } catch {
+      // nope!
+    }
+
+    if (!object || !stringified) {
+      return [['tag', '(unknown)']];
+    }
+
+    {
+      // <my-app@component:foo-bar::ember123>
+      let match = stringified.match(/<([a-z0-9-_]+)@([a-z0-9-_]+):([a-z0-9-_]+)::([a-z0-9-_]+)>/i);
+
+      if (match) {
+        return [
+          ['tag', '<'],
+          ['namespace', match[1]],
+          ['tag', '@'],
+          ['namespace', match[2]],
+          ['tag', ':'],
+          ['name', match[3]],
+          ['tag', '::'],
+          ['id', match[4]],
+          ['tag', '>']
+        ];
+      }
+    }
+
+    // TODO: support other ember object strings, `[object Object]`, `Symbol(hi)` etc
+    return [['tag', stringified]];
+  }
+
+  _insertHTML(html) {
+    document.body.insertAdjacentHTML('beforeend', html.trim());
+    return document.body.lastChild;
+  }
+
+  _insertStylesheet(content) {
+    let style = document.createElement('style');
+    style.appendChild(document.createTextNode(content));
+    document.head.appendChild(style);
+  }
+}
--- ember_debug/view-debug.js
+++ ember_debug/view-debug.js
@@ -1,23 +1,17 @@
 /* eslint no-cond-assign:0 */
 import PortMixin from 'ember-debug/mixins/port-mixin';
-import GlimmerTree from 'ember-debug/libs/glimmer-tree';
-import { typeOf } from './utils/type-check';
+import RenderTree from 'ember-debug/libs/render-tree';
+import ViewInspection from 'ember-debug/libs/view-inspection';
 
 const Ember = window.Ember;
 
 const {
-  guidFor,
   computed,
   run,
   Object: EmberObject,
-  Component,
-  String
 } = Ember;
 const { throttle } = run;
 const { readOnly } = computed;
-const { classify } = String;
-
-let layerDiv, previewDiv;
 
 export default EmberObject.extend(PortMixin, {
   namespace: null,
@@ -32,14 +26,15 @@ export default EmberObject.extend(PortMixin, {
       this.sendTree();
     },
     hideLayer() {
-      this.hideLayer();
+      // this.hideLayer();
     },
-    previewLayer(message) {
-      this.glimmerTree.highlightLayer(message.objectId || message.elementId, true);
+    previewLayer() {
+      // this.renderTree.highlightLayer(message.objectId || message.elementId, true);
     },
     hidePreview() {
-      this.hidePreview();
+      // this.hidePreview();
     },
+
     inspectViews(message) {
       if (message.inspect) {
         this.startInspecting();
@@ -48,24 +43,26 @@ export default EmberObject.extend(PortMixin, {
       }
     },
 
-    scrollToElement({ elementId }) {
-      let el = document.querySelector(`#${elementId}`);
-      if (el) {
-        el.scrollIntoView();
-      }
+    scrollToElement() {
+      // let el = document.querySelector(`#${elementId}`);
+      // if (el) {
+      //   el.scrollIntoView();
+      // }
     },
 
-    inspectElement({ objectId, elementId }) {
-      if (objectId) {
-        this.inspectViewElement(objectId);
-      } else {
-        let element = document.getElementById(elementId);
-        this.inspectElement(element);
-      }
+    inspectElement() {
+      // if (objectId) {
+      //   this.inspectViewElement(objectId);
+      // } else {
+      //   let element = document.getElementById(elementId);
+      //   this.inspectElement(element);
+      // }
     },
 
     contextMenu() {
-      this.inspectComponentForNode(this.lastClickedElement);
+      let { lastRightClicked } = this;
+      this.lastRightClicked = null;
+      this.inspectNearest(lastRightClicked);
     }
   },
 
@@ -73,82 +70,71 @@ export default EmberObject.extend(PortMixin, {
     this._super(...arguments);
 
     this.retainedObjects = [];
-    this._durations = {};
-    layerDiv = document.createElement('div');
-    layerDiv.setAttribute('data-label', 'layer-div');
-    layerDiv.style.display = 'none';
-    document.body.appendChild(layerDiv);
 
-    previewDiv = document.createElement('div');
-    previewDiv.style.pointerEvents = 'none';
-    previewDiv.style.display = 'none';
-    previewDiv.setAttribute('data-label', 'preview-div');
-    document.body.appendChild(previewDiv);
-
-    // Store last clicked element for context menu
-    this.lastClickedHandler = (event) => {
-      if (event.button === 2) {
-        this.lastClickedElement = event.target;
-      }
-    };
-    window.addEventListener('mousedown', this.lastClickedHandler);
-
-    this.resizeHandler = () => {
-      this.hideLayer();
-    };
-    window.addEventListener('resize', this.resizeHandler);
-
-    this.glimmerTree = new GlimmerTree({
+    let renderTree = this.renderTree = new RenderTree({
       owner: this.getOwner(),
       retainObject: this.retainObject.bind(this),
-      highlightRange: this._highlightRange.bind(this),
-      objectInspector: this.get('objectInspector'),
-      durations: this._durations,
-      viewRegistry: this.get('viewRegistry')
     });
+
+    this.viewInspection = new ViewInspection({
+      renderTree,
+      objectInspector: this.objectInspector,
+    });
+
+    this.setupListeners();
   },
 
-  inspectComponentForNode(domNode) {
-    let viewElem = this.findNearestView(domNode);
-    if (!viewElem) {
-      this.get('adapter').log('No Ember component found.');
-      return;
-    }
+  setupListeners() {
+    this.lastRightClicked = null;
+    this.onRightClick = this.onRightClick.bind(this);
+    window.addEventListener('mousedown', this.onRightClick);
 
-    this.sendMessage('inspectComponent', {
-      viewId: viewElem.id
-    });
+    this.onResize = this.onResize.bind(this);
+    window.addEventListener('resize', this.resizeHandler);
   },
 
-  updateDurations(durations) {
-    for (let guid in durations) {
-      if (!durations.hasOwnProperty(guid)) {
-        continue;
-      }
-      this._durations[guid] = durations[guid];
-    }
+  cleanupListeners() {
+    this.lastRightClicked = null;
+    window.removeEventListener('mousedown', this.onRightClick);
 
-    this.glimmerTree.updateDurations(this._durations);
+    window.removeEventListener('resize', this.onResize);
+  },
+
+  onRightClick(event) {
+    if (event.button === 2) {
+      this.lastRightClicked = event.target;
+    }
+  },
+
+  onResize() {
+    // TODO hide or redraw highlight/tooltip
+  },
+
+  inspectNearest(node) {
+    if (!this.viewInspection.inspectNearest(node)) {
+      this.adapter.log('No Ember component found.');
+    }
   },
 
   retainObject(object) {
-    this.retainedObjects.push(object);
-    return this.get('objectInspector').retainObject(object);
+    let guid = this.objectInspector.retainObject(object);
+    this.retainedObjects.push(guid);
+    return guid;
   },
 
   releaseCurrentObjects() {
-    this.retainedObjects.forEach(item => {
-      this.get('objectInspector').releaseObject(guidFor(item));
+    this.retainedObjects.forEach(guid => {
+      this.objectInspector.releaseObject(guid);
     });
     this.retainedObjects = [];
   },
 
   willDestroy() {
     this._super();
-    window.removeEventListener('resize', this.resizeHandler);
-    window.removeEventListener('mousedown', this.lastClickedHandler);
-    document.body.removeChild(layerDiv);
-    document.body.removeChild(previewDiv);
+
+    this.cleanupLayers();
+    this.cleanupListeners();
+
     this.releaseCurrentObjects();
     this.stopInspecting();
   },
@@ -175,253 +161,45 @@ export default EmberObject.extend(PortMixin, {
     run.scheduleOnce('afterRender', this, this.scheduledSendTree);
   },
 
-  startInspecting() {
-    let viewElem = null;
-    this.sendMessage('startInspecting', {});
-
-    // we don't want the preview div to intercept the mousemove event
-    previewDiv.style.pointerEvents = 'none';
-
-    let pinView = () => {
-      if (viewElem) {
-        this.glimmerTree.highlightLayer(viewElem.id);
-
-        let view = this.get('objectInspector').sentObjects[viewElem.id];
-        if (view instanceof Component) {
-          this.get('objectInspector').sendObject(view);
-          this.sendMessage('inspectComponent', { viewId: viewElem.id });
-        }
-      }
-      this.stopInspecting();
-      return false;
-    };
-
-    this.mousemoveHandler = (e) => {
-      viewElem = this.findNearestView(e.target);
-
-      if (viewElem) {
-        this.glimmerTree.highlightLayer(viewElem.id, true);
-      }
-    };
-    this.mousedownHandler = () => {
-      // prevent app-defined clicks from being fired
-      previewDiv.style.pointerEvents = '';
-      previewDiv.addEventListener('mouseup', () => pinView(), { once: true });
-    };
-    this.mouseupHandler = () => pinView();
-    document.body.addEventListener('mousemove', this.mousemoveHandler);
-    document.body.addEventListener('mousedown', this.mousedownHandler);
-    document.body.addEventListener('mouseup', this.mouseupHandler);
-    document.body.style.cursor = '-webkit-zoom-in';
-  },
-
-  findNearestView(elem) {
-    if (!elem) {
-      return null;
-    }
-    if (elem.classList.contains('ember-view')) {
-      return elem;
-    }
-    return this.findNearestView(elem.closest('.ember-view'));
-  },
-
-  stopInspecting() {
-    document.body.removeEventListener('mousemove', this.mousemoveHandler);
-    document.body.removeEventListener('mousedown', this.mousedownHandler);
-    document.body.removeEventListener('mouseup', this.mouseupHandler);
-    document.body.style.cursor = '';
-    this.hidePreview();
-    this.sendMessage('stopInspecting', {});
-  },
-
-  send() {
-    if (this.isDestroying) {
-      return;
-    }
-    this.releaseCurrentObjects();
-    let tree = this.viewTree();
-    if (tree) {
-      this.sendMessage('viewTree', { tree });
-    }
-  },
-
   scheduledSendTree() {
     // needs to trigger on the trailing edge of the wait interval, otherwise it might happen
     // that we do not pick up fast route switching or 2 or more short rerenders
     throttle(this, this.send, 250, false);
   },
 
-  viewTree() {
-    let emberApp = this.getOwner();
-    if (!emberApp) {
-      return false;
+  send() {
+    if (this.isDestroying) {
+      return;
     }
 
-    return this.glimmerTree.build();
+    this.getTree();
+    // this.sendMessage('viewTree', this.getTree());
+  },
+
+  getTree() {
+    this.releaseCurrentObjects();
+
+    // TODO: why is this needed?
+    let emberApp = this.getOwner();
+
+    if (!emberApp) {
+      return [];
+    }
+
+    return this.renderTree.build();
+  },
+
+  startInspecting() {
+    this.sendMessage('startInspecting', {});
+    this.viewInspection.start();
+  },
+
+  stopInspecting() {
+    this.viewInspection.stop();
+    this.sendMessage('stopInspecting', {});
   },
 
   getOwner() {
-    return this.get('namespace.owner');
-  },
-
-  // TODO: This method needs a serious refactor/cleanup
-  _highlightRange(rect, options) {
-    let div;
-    let isPreview = options.isPreview;
-
-    // take into account the scrolling position as mentioned in docs
-    // https://developer.mozilla.org/en-US/docs/Web/API/element.getBoundingClientRect
-    let styles = {
-      display: 'block',
-      position: 'absolute',
-      backgroundColor: 'rgba(255, 255, 255, 0.7)',
-      border: '2px solid rgb(102, 102, 102)',
-      padding: '0',
-      right: 'auto',
-      direction: 'ltr',
-      boxSizing: 'border-box',
-      color: 'rgb(51, 51, 255)',
-      fontFamily: 'Menlo, sans-serif',
-      minHeight: '63px',
-      zIndex: 10000,
-      top: `${rect.top + window.scrollY}px`,
-      left: `${rect.left + window.scrollX}px`,
-      width: `${rect.width}px`,
-      height: `${rect.height}px`
-    };
-
-    if (isPreview) {
-      div = previewDiv;
-    } else {
-      this.hideLayer();
-      div = layerDiv;
-      this.hidePreview();
-    }
-    for (let prop in styles) {
-      div.style[prop] = styles[prop];
-    }
-    let output = '';
-
-    if (!isPreview) {
-      output = '<span class=\'close\' data-label=\'layer-close\'>&times;</span>';
-    }
-
-    let template = options.template;
-
-    if (template) {
-      output += `<p class='template'><span>template</span>=<span data-label='layer-template'>${escapeHTML(template.name)}</span></p>`;
-    }
-    let view = options.view;
-    let controller = options.controller;
-    if (!view || !(view.object instanceof Component)) {
-      if (controller) {
-        output += `<p class='controller'><span>controller</span>=<span data-label='layer-controller'>${escapeHTML(controller.name)}</span></p>`;
-      }
-      if (view) {
-        output += `<p class='view'><span>view</span>=<span data-label='layer-view'>${escapeHTML(view.name)}</span></p>`;
-      }
-    } else {
-      output += `<p class='component'><span>component</span>=<span data-label='layer-component'>${classify(escapeHTML(view.name)).replace(/\//g, '::')}</span></p>`;
-    }
-
-    let model = options.model;
-    if (model) {
-      output += `<p class='model'><span>model</span>=<span data-label='layer-model'>${escapeHTML(model.name)}</span></p>`;
-    }
-    div.innerHTML = output;
-
-    for (let p of div.querySelectorAll('p')) {
-      p.style.float = 'left';
-      p.style.margin = 0;
-      p.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-      p.style.padding = '5px';
-      p.style.color = 'rgb(0, 0, 153)';
-    }
-    for (let p of div.querySelectorAll('p.model')) {
-      p.style.clear = 'left';
-    }
-    for (let p of div.querySelectorAll('p span:first-child')) {
-      p.style.color = 'rgb(153, 153, 0)';
-    }
-    for (let p of div.querySelectorAll('p span:last-child')) {
-      p.style.color = 'rgb(153, 0, 153)';
-    }
-
-    if (!isPreview) {
-      let cancelEvent = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-      };
-      for (let span of div.querySelectorAll('span.close')) {
-        span.style.float = 'right';
-        span.style.margin = '5px';
-        span.style.background = '#666';
-        span.style.color = '#eee';
-        span.style.fontFamily = 'helvetica, sans-serif';
-        span.style.fontSize = '14px';
-        span.style.width = '16px';
-        span.style.height = '16px';
-        span.style.lineHeight = '14px';
-        span.style.borderRadius = '16px';
-        span.style.textAlign = 'center';
-        span.style.cursor = 'pointer';
-        span.style.opacity = '0.5';
-        span.style.fontWeight = 'normal';
-        span.style.textShadow = 'none';
-        span.addEventListener('click', (e) => {
-          cancelEvent(e);
-          this.hideLayer();
-        });
-        span.addEventListener('mouseup', cancelEvent);
-        span.addEventListener('mousedown', cancelEvent);
-      }
-    }
-
-    this._addClickListeners(div, view, 'component');
-    this._addClickListeners(div, controller, 'controller');
-    this._addClickListeners(div, view, 'view');
-
-    for (let span of div.querySelectorAll('p.template span:last-child')) {
-      span.style.cursor = 'pointer';
-      span.addEventListener('click', () => {
-        if (view) {
-          this.inspectViewElement(guidFor(view.object));
-        } else if (options.element) {
-          this.inspectElement(options.element);
-        }
-      });
-    }
-
-
-    if (model && model.object && ((model.object instanceof EmberObject) || typeOf(model.object) === 'array')) {
-      this._addClickListeners(div, model, 'model');
-    }
-  },
-
-  hideLayer() {
-    layerDiv.style.display = 'none';
-  },
-
-  hidePreview() {
-    previewDiv.style.display = 'none';
-  },
-
-  _addClickListeners(div, item, selector) {
-    for (let span of div.querySelectorAll(`p.${selector} span:last-child`)) {
-      span.style.cursor = 'pointer';
-      span.addEventListener('click', () => {
-        this.get('objectInspector').sendObject(item.object);
-      });
-    }
-  },
-
-  viewRegistry: computed('namespace.owner', function() {
-    return this.getOwner().lookup('-view-registry:main');
-  }),
+    return this.namespace.owner;
+  }
 });
-
-function escapeHTML(string) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(string));
-  return div.innerHTML;
-}
