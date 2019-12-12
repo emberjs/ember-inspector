0302acf2cd6e35bf9c40cfc90d5b872443febad7
Robert Wagner <rwwagner90@gmail.com>
2019-12-12 16:08:43 -0500

    Delete dasherize.js

--- app/helpers/dasherize.js
+++
@@ -1,8 +0,0 @@
-import { dasherize } from '@ember/string';
-import { helper } from '@ember/component/helper';
-
-export function dasherizeString([str]) {
-  return dasherize(str).replace(/\//g, '::');
-}
-
-export default helper(dasherizeString);
