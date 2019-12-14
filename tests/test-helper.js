7712164acb42dd76dbbbd5451fa39a4ac9fb82b2
Godfrey Chan <godfreykfc@gmail.com>
2019-12-14 14:04:34 -0800

    Refactor `StorageService` (#1095)
    Ensure we are always using the in-memory storage service backend in
    tests, so that we aren't leaking test states into local storage, or
    otherwise change test behaviors due to existing items there.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,19 +1,26 @@
-import Application from '../app';
-import Ember from 'ember';
+import Application from 'ember-inspector/app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-const { generateGuid } = Ember;
-
-setApplication(Application.create(config.APP));
-window.NO_EMBER_DEBUG = true;
-start();
 
 Application.instanceInitializer({
-  name: `${generateGuid()}-detectEmberApplication`,
+  name: '00-force-memory-storage-backend',
+  initialize(instance) {
+    let memory = instance.lookup('service:storage/memory');
+    let storage = instance.lookup('service:storage');
+    storage.backend = memory;
+  }
+});
+
+Application.instanceInitializer({
+  name: '01-detect-ember-application',
   initialize(instance) {
     instance.lookup('route:app-detected').reopen({
       model() { }
     });
   }
 });
+
+setApplication(Application.create(config.APP));
+window.NO_EMBER_DEBUG = true;
+start();
