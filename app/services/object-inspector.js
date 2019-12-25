68cc7f6f569115605e03b5ba4a1b6f9b81e6a97c
betocantu93 <betocantu93@gmail.com>
2019-12-25 14:22:03 -0600

    object inspector toggling and state in a service

---
+++ app/services/object-inspector.js
@@ -0,0 +1,40 @@
+import Service, { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { schedule } from "@ember/runloop";
+
+export default class ObjectInspectorService extends Service {
+  @service("layout") layoutService;
+
+  @tracked inspectorWidth = 360;
+  @tracked inspectorExpanded = false;
+
+  @action
+  showInspector() {
+    if (this.inspectorExpanded === false) {
+      this.inspectorExpanded = true;
+      schedule("afterRender", () => {
+        this.layoutService.trigger("resize", { source: "object-inspector" });
+      });
+    }
+  }
+
+  @action
+  hideInspector() {
+    if (this.inspectorExpanded === true) {
+      this.inspectorExpanded = false;
+      schedule("afterRender", () => {
+        this.layoutService.trigger("resize", { source: "object-inspector" });
+      });
+    }
+  }
+
+  @action
+  toggleInspector() {
+    if (this.inspectorExpanded) {
+      this.hideInspector();
+    } else {
+      this.showInspector();
+    }
+  }
+}
