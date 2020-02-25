b2f1eefe9dc1d6c8f48fd65d3e43a90993623ffc
Windvis <sam.van.campenhout@gmail.com>
2020-02-25 15:34:19 +0100

    Update to Ember 3.16

--- testem.js
+++ testem.js
@@ -8,7 +8,7 @@ module.exports = {
   launch_in_dev: [
     'Chrome'
   ],
-  browser_start_timeout: process.env.CI ? 60 : undefined,
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
       ci: [
