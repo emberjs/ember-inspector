3cf06a293eead094bb585987c0e64d1f087d9edf
Robert Wagner <rwwagner90@gmail.com>
2020-04-18 13:20:46 -0400

    Fix template lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     'link-rel-noopener': false,
     'no-inline-styles': false,
     'no-invalid-interactive': false,
+    'no-negated-condition': false,
+    'no-positive-tabindex': false,
     'table-groups': false
   }
 };
