5d4364556d41fc2c9a92a6b9403b36181533eb95
Sarah Yu <sarahmyu15@gmail.com>
2020-04-30 04:53:56 -0400

    feat: allow object arguments in component tree to be inspected (#1175)
    * feat: allow object arguments in component tree to be inspected
    
    * Address feedback
    
    * Tweak styles
    
    * Update colors
    
    Co-authored-by: Robert Wagner <rwwagner90@gmail.com>

---
+++ app/components/component-tree-arg.js
@@ -0,0 +1,30 @@
+import Component from '@glimmer/component';
+import truncate from 'ember-inspector/utils/truncate';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class ComponentTreeArg extends Component {
+  @service port;
+
+  get isObject() {
+    return typeof this.args.value === 'object' && this.args.value !== null;
+  }
+
+  get displayValue() {
+    if (this.isObject) {
+      return '...';
+    } else if (typeof this.args.value === 'string') {
+      // Escape any interior quotes – we will add the surrounding quotes in the template
+      return `"${truncate(this.args.value.replace(/"/g, '\\"'))}"`;
+    }
+
+    return String(this.args.value);
+  }
+
+  @action inspect(event) {
+    event.stopPropagation();
+    this.port.send('objectInspector:inspectById', {
+      objectId: this.args.value.id
+    });
+  }
+}
