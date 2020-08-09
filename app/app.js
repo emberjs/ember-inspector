31d4b833047d41d1e4cfee2fcf8c13475f3e79c9
Robert Wagner <rwwagner90@gmail.com>
2020-08-09 12:09:48 -0400

    ember-cli 3.20

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config from 'ember-inspector/config/environment';
 
 const version = '{{EMBER_INSPECTOR_VERSION}}';
 
