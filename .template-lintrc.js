e2066e602237ab9ee5dd9f2305f5deaa05daaac6
Robert Wagner <rwwagner90@gmail.com>
2021-05-15 16:53:48 -0400

    Fix some template lint issues

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,16 +3,14 @@
 module.exports = {
   extends: 'recommended',
   rules: {
-    'link-rel-noopener': false,
-    'no-action': false,
     'no-down-event-binding': false,
+    'no-negated-condition': false,
+
+    // TODO: enable these rules
+    'no-action': false,
     'no-implicit-this': false,
     'no-inline-styles': false,
-    'no-negated-condition': false,
     'no-positive-tabindex': false,
-    'no-unknown-arguments-for-builtin-components': false,
-    'no-yield-only': false,
-    'require-input-label': false,
     'table-groups': false,
   },
 };
