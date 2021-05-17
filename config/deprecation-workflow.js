11a955b83858e50b691d1687721190891fd4af9b
Robert Wagner <rwwagner90@gmail.com>
2021-05-16 21:45:41 -0400

    Silence deprecations for now

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,19 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: 'silence', matchId: 'ember-global' },
+    { handler: 'silence', matchId: 'ember.built-in-components.import' },
+    { handler: 'silence', matchId: 'ember.globals-resolver' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'implicit-injections' },
+    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'silence', matchId: 'route-render-template' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'globals-resolver' },
+  ],
+};
