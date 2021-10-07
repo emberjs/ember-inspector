b1efd76e7f39ea09439317b3392f218260407591
Robert Wagner <rwwagner90@gmail.com>
2021-10-07 16:42:26 -0400

    Update ember-flatpickr

--- app/components/date-property-field.js
+++ app/components/date-property-field.js
@@ -1,20 +1,23 @@
 import { scheduleOnce } from '@ember/runloop';
+import { action } from '@ember/object';
 import DatePicker from 'ember-inspector/components/ember-flatpickr';
-import { KEYS } from 'ember-inspector/utils/key-codes';
+// import { KEYS } from 'ember-inspector/utils/key-codes';
 
 export default class DatePropertyFieldComponent extends DatePicker {
-  didInsertElement() {
-    super.didInsertElement(...arguments);
+  @action
+  onInsert(element) {
+    super.onInsert(element);
 
     scheduleOnce('afterRender', this, this._openFlatpickr);
   }
 
-  keyUp(e) {
-    if (e.keyCode === KEYS.escape) {
-      this.flatpickrRef.close();
-    }
-    return super.keyUp(...arguments);
-  }
+  // TODO: Add back close with esc key support
+  // keyUp(e) {
+  //   if (e.keyCode === KEYS.escape) {
+  //     this.flatpickrRef.close();
+  //   }
+  //   return super.keyUp(...arguments);
+  // }
 
   _openFlatpickr() {
     this.flatpickrRef.open();
