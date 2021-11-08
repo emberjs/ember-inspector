ffef38752cdf6a98567fb03110f19bea0fd2fa72
Lukas Kohler <lux@ontheblueplanet.com>
2021-11-08 22:38:26 +0100

    fix startup bug in Firefox (fixes #1770) (#1780)
    * fix startup bug in Firefox (fixes #1770)
    
    Co-authored-by: Godfrey Chan <godfreykfc@gmail.com>

--- ember_debug/utils/rsvp.js
+++ ember_debug/utils/rsvp.js
@@ -5,6 +5,12 @@ let module, RSVP;
 try {
   module = requireModule('rsvp');
   RSVP = module.default;
+
+  // The RSVP module should have named exports for `Promise`, etc,
+  // but some old versions do not and provide `RSVP.Promise`, etc.
+  if (!('Promise' in module)) {
+    module = RSVP;
+  }
 } catch {
   module = RSVP = Ember.RSVP;
 }
