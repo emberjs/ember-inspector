38fe8e1c7a265b7bafdcca1e14673dc761412d46
Geordan Neukum <gneukum1@gmail.com>
2022-04-27 20:29:28 -0400

    object-inspector-test: fix legacy-attribute-arguments issue
    Right now, when we run the 'Views are correctly handled when destroyed
    during transitions' test from the 'Ember Debug - Object Inspector'
    suite, there is a deprecation logged along the lines of:
    
        Passing the `@class` argument to <Input> is deprecated. Instead,
        please pass the attribute directly, i.e. `<Input class={{...}} />`
        instead of `<Input @class={{...}} />` or `{{input class=...}}`.
    
    Let's fix this deprecation by preferring angle bracket syntax for the
    input component. This will help avoid having an unnecessary deprecation
    show up in the test output.

--- tests/ember_debug/object-inspector-test.js
+++ tests/ember_debug/object-inspector-test.js
@@ -96,7 +96,7 @@ module('Ember Debug - Object Inspector', function (hooks) {
     this.owner.register('component:x-simple', Component);
     this.owner.register(
       'template:simple',
-      hbs`Simple {{input class="simple-input"}} {{x-simple class="simple-view"}}`
+      hbs`Simple <Input class="simple-input"/> {{x-simple class="simple-view"}}`
     );
 
     objectInspector = EmberDebug.get('objectInspector');
