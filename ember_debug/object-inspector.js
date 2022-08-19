cd1f51a41bca01fc972d96d34cf5d88178b323fb
patricklx <patricklx@users.noreply.github.com>
2022-08-19 09:59:51 +0200

    fix lint

--- ember_debug/object-inspector.js
+++ ember_debug/object-inspector.js
@@ -788,7 +788,7 @@ function getClassName(object) {
 
     if (
       constructor.toString &&
-      constructor.toString !== Object.prototype.toString && 
+      constructor.toString !== Object.prototype.toString &&
       constructor.toString !== Function.prototype.toString
     ) {
       name = constructor.toString();
