cbc7e9432682a461c69b625ac47f7d0b7c661ac2
Robert Wagner <rwwagner90@gmail.com>
2022-08-30 17:04:13 -0400

    Update to ember 4 (#2124)
    * Update to ember 4
    
    * Add @ember/legacy-built-in-components

--- app/components/object-inspector/property-field.js
+++ app/components/object-inspector/property-field.js
@@ -1,4 +1,4 @@
-import TextField from '@ember/component/text-field';
+import { TextField } from '@ember/legacy-built-in-components';
 
 export default class PropertyField extends TextField {
   didInsertElement() {
