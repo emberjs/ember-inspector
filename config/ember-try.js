60e05f8f218e2dc7452808d520527074d13d7302
Robert Wagner <rwwagner90@gmail.com>
2022-08-30 22:33:42 -0400

    Target Ember 3.16+ for main branch (#2128)
    * Update Ember version support
    
    * Update build.yml

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,14 +13,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.16',
           npm: {
             devDependencies: {
