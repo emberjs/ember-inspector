9ff289146bae32f6f728af933f4d761ec1a2ee0e
patrick <pa.trickjlp@gmail.com>
2023-08-31 11:42:17 +0200

    fix

--- ember_debug/utils/ember/loader.js
+++ ember_debug/utils/ember/loader.js
@@ -1,22 +1,22 @@
 import Ember from '../ember';
 
-let module;
-
-try {
-  module = Ember.__loader;
-} catch {
-  module = {
-    require,
-    requireModule,
-  };
-}
+const require = function (id) {
+  try {
+    return Ember.__loader.require(id);
+  } catch (e) {
+    return requireModule(id);
+  }
+};
 
 export function emberSafeRequire(id) {
   try {
-    return module.require(id);
+    return require(id);
   } catch (e) {
     return undefined;
   }
 }
 
-export let EmberLoader = module;
+export let EmberLoader = {
+  require,
+  requireModule: require,
+};
