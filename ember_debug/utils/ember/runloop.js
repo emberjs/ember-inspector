c52447afd53ae9adb71ff1219e82a4e35fc41d48
patrick <pa.trickjlp@gmail.com>
2023-08-31 14:08:03 +0200

    fix

--- ember_debug/utils/ember/runloop.js
+++ ember_debug/utils/ember/runloop.js
@@ -1,16 +1,25 @@
 import Ember from '../ember';
 import * as runloop from '@ember/runloop';
 
+let module = runloop;
+
 export let run = runloop.run;
 export let _backburner;
 
+const keys = ['cancel', 'debounce', 'join', 'later', 'scheduleOnce'];
+
 try {
-  const module = requireModule('@ember/runloop');
+  module = requireModule('@ember/runloop');
   // it could happen that runloop is available but _backburner is not exported (dead code)
   // then we need to use our own
   _backburner = module._backburner || runloop._backburner;
 } catch {
   _backburner = Ember?.run?.backburner || runloop._backburner;
+  module = Ember?.run || module;
+}
+
+if (!keys.every((k) => k in module)) {
+  module = runloop;
 }
 
 // if it is our own, run a internal to trigger `end`
