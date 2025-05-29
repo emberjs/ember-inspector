7481380842e15ba3d735e148db3b0caaf190c5ba
Chris Manson <chris@manson.ie>
2025-05-29 15:41:16 +0100

    fix bug redefining exports with the same name in AMD

--- ember_debug/utils/ember.js
+++ ember_debug/utils/ember.js
@@ -9,87 +9,89 @@ try {
 }
 
 let {
-  ArrayProxy,
-  Namespace,
-  ActionHandler,
-  ControllerMixin,
-  CoreObject,
-  Application,
-  MutableArray,
-  MutableEnumerable,
-  NativeArray,
-  Component,
-  Observable,
-  Evented,
-  PromiseProxyMixin,
-  Service,
-  Object: EmberObject,
-  ObjectProxy,
-  VERSION,
-  ComputedProperty,
-  meta,
-  get,
-  set,
-  computed,
-  _captureRenderTree: captureRenderTree,
+  ArrayProxy: EmberArrayProxy,
+  Namespace: EmberNamespace,
+  ActionHandler: EmberActionHandler,
+  ControllerMixin: EmberControllerMixin,
+  CoreObject: EmberCoreObject,
+  Application: EmberApplication,
+  MutableArray: EmberMutableArray,
+  MutableEnumerable: EmberMutableEnumerable,
+  NativeArray: EmberNativeArray,
+  Component: EmberComponent,
+  Observable: EmberObservable,
+  Evented: EmberEvented,
+  PromiseProxyMixin: EmberPromiseProxyMixin,
+  Service: EmberService,
+  Object: InternalEmberObject,
+  ObjectProxy: EmberObjectProxy,
+  VERSION: EmberVERSION,
+  ComputedProperty: EmberComputedProperty,
+  meta: Embermeta,
+  get: Emberget,
+  set: Emberset,
+  computed: Embercomputed,
+  _captureRenderTree: EmbercaptureRenderTree,
 } = Ember || {};
 
 let getEnv = () => Ember.ENV;
 
 if (!Ember) {
-  captureRenderTree = emberSafeRequire('@ember/debug')?.captureRenderTree;
+  EmbercaptureRenderTree = emberSafeRequire('@ember/debug')?.captureRenderTree;
   getEnv = emberSafeRequire('@ember/-internals/environment')?.getENV;
-  ArrayProxy = emberSafeRequire('@ember/array/proxy')?.default;
-  ObjectProxy = emberSafeRequire('@ember/object/proxy')?.default;
-  MutableArray = emberSafeRequire('@ember/array/mutable')?.default;
-  Namespace = emberSafeRequire('@ember/application/namespace')?.default;
-  MutableEnumerable = emberSafeRequire('@ember/enumerable/mutable')?.default;
-  NativeArray = emberSafeRequire('@ember/array')?.NativeArray;
-  ControllerMixin = emberSafeRequire('@ember/controller')?.ControllerMixin;
-  CoreObject = emberSafeRequire('@ember/object/core')?.default;
-  Application = emberSafeRequire('@ember/application')?.default;
-  Component = emberSafeRequire('@ember/component')?.default;
-  Observable = emberSafeRequire('@ember/object/observable')?.default;
-  Evented = emberSafeRequire('@ember/object/evented')?.default;
-  PromiseProxyMixin = emberSafeRequire(
+  EmberArrayProxy = emberSafeRequire('@ember/array/proxy')?.default;
+  EmberObjectProxy = emberSafeRequire('@ember/object/proxy')?.default;
+  EmberMutableArray = emberSafeRequire('@ember/array/mutable')?.default;
+  EmberNamespace = emberSafeRequire('@ember/application/namespace')?.default;
+  EmberMutableEnumerable = emberSafeRequire(
+    '@ember/enumerable/mutable',
+  )?.default;
+  EmberNativeArray = emberSafeRequire('@ember/array')?.NativeArray;
+  EmberControllerMixin = emberSafeRequire('@ember/controller')?.ControllerMixin;
+  EmberCoreObject = emberSafeRequire('@ember/object/core')?.default;
+  EmberApplication = emberSafeRequire('@ember/application')?.default;
+  EmberComponent = emberSafeRequire('@ember/component')?.default;
+  EmberObservable = emberSafeRequire('@ember/object/observable')?.default;
+  EmberEvented = emberSafeRequire('@ember/object/evented')?.default;
+  EmberPromiseProxyMixin = emberSafeRequire(
     '@ember/object/promise-proxy-mixin',
   )?.default;
-  Service = emberSafeRequire('@ember/service')?.default;
-  EmberObject = emberSafeRequire('@ember/object')?.default;
-  VERSION = emberSafeRequire('ember/version')?.default;
-  ComputedProperty = emberSafeRequire(
+  EmberService = emberSafeRequire('@ember/service')?.default;
+  InternalEmberObject = emberSafeRequire('@ember/object')?.default;
+  EmberVERSION = emberSafeRequire('ember/version')?.default;
+  EmberComputedProperty = emberSafeRequire(
     '@ember/-internals/metal',
   )?.ComputedProperty;
-  meta = emberSafeRequire('@ember/-internals/meta')?.meta;
-  set = emberSafeRequire('@ember/object')?.set;
-  get = emberSafeRequire('@ember/object')?.get;
+  Embermeta = emberSafeRequire('@ember/-internals/meta')?.meta;
+  Emberset = emberSafeRequire('@ember/object')?.set;
+  Emberget = emberSafeRequire('@ember/object')?.get;
 }
 
 export {
-  ArrayProxy,
-  Namespace,
-  ActionHandler,
-  Application,
-  ControllerMixin,
-  MutableArray,
-  MutableEnumerable,
-  NativeArray,
-  CoreObject,
-  ObjectProxy,
-  Component,
-  Observable,
-  Evented,
-  Service,
-  PromiseProxyMixin,
-  EmberObject,
-  VERSION,
-  ComputedProperty,
-  meta,
-  computed,
-  get,
-  set,
-  captureRenderTree,
-  getEnv,
+  EmberArrayProxy as ArrayProxy,
+  EmberNamespace as Namespace,
+  EmberActionHandler as ActionHandler,
+  EmberApplication as Application,
+  EmberControllerMixin as ControllerMixin,
+  EmberMutableArray as MutableArray,
+  EmberMutableEnumerable as MutableEnumerable,
+  EmberNativeArray as NativeArray,
+  EmberCoreObject as CoreObject,
+  EmberObjectProxy as ObjectProxy,
+  EmberComponent as Component,
+  EmberObservable as Observable,
+  EmberEvented as Evented,
+  EmberService as Service,
+  EmberPromiseProxyMixin as PromiseProxyMixin,
+  InternalEmberObject as EmberObject,
+  EmberVERSION as VERSION,
+  EmberComputedProperty as ComputedProperty,
+  Embermeta as meta,
+  Embercomputed as computed,
+  Emberget as get,
+  Emberset as set,
+  EmbercaptureRenderTree as captureRenderTree,
+  getEnv as getEnv,
 };
 
 export default Ember;
