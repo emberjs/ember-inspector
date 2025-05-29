5c2be43dcef546beef112053f1b5ed546c077f98
Chris Manson <chris@manson.ie>
2025-05-29 15:40:40 +0100

    add each of the entrypoints to ember-debug

--- ember_debug/rollup.config.js
+++ ember_debug/rollup.config.js
@@ -1,15 +1,48 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
+import { globSync } from 'glob';
 
 export default {
-  input: 'main.js',
+  input: [
+    'main.js',
+    ...globSync('adapters/**/*.js'),
+    'utils/version.js',
+    'port.js',
+    'utils/ember.js',
+    'models/profile-node.js',
+    'libs/promise-assembler.js',
+  ],
   output: {
     format: 'amd',
     amd: {
-      id: 'ember-debug/main',
+      autoId: true,
+      // id: 'ember-debug/[name]',
+      basePath: 'ember-debug',
     },
+    chunkFileNames: '[name].js',
     dir: 'dist',
   },
-  plugins: [babel(), nodeResolve(), commonjs()],
+
+  plugins: [
+    babel(),
+    nodeResolve(),
+    commonjs(),
+    /**
+     * this plugin forces each of the intenral dependencies for each of the chunks to be prefixed with ember-debug
+     */
+    {
+      name: 'rollup-plugin-name-amd-modules',
+      renderChunk(code) {
+        let splitCode = code.split('\n');
+
+        splitCode[0] = splitCode[0].replaceAll(
+          /'\.\/([^']*)'/g,
+          `'ember-debug/$1'`,
+        );
+
+        return splitCode.join('\n');
+      },
+    },
+  ],
 };
