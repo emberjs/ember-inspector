50091151b098f5c4fa61ddebe01c81e181a7070a
Patrick Pircher <patricklx@users.noreply.github.com>
2025-05-31 18:11:06 +0200

    range

--- ember_debug_test_app/tests/ember_debug/view-debug-test.js
+++ ember_debug_test_app/tests/ember_debug/view-debug-test.js
@@ -797,7 +797,7 @@ module('Ember Debug - View', function (hooks) {
       Component(
         {
           name: 'in-element',
-          bounds: 'single',
+          bounds: 'range',
           args: (actual) => {
             QUnit.assert.ok(actual.positional[0]);
             async function testArgsValue() {
@@ -891,7 +891,7 @@ module('Ember Debug - View', function (hooks) {
       }),
       Component(
         {
-          bounds: 'single',
+          bounds: 'range',
           name: 'in-element',
           args: Args({ names: [], positionals: 1 }),
           template: null,
