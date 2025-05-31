62a75c7d0b4af27d177a55fa0ce68d6f7b62f09d
Patrick Pircher <patricklx@users.noreply.github.com>
2025-05-31 14:36:51 +0200

    lint

--- ember_debug_test_app/tests/ember_debug/view-debug-test.js
+++ ember_debug_test_app/tests/ember_debug/view-debug-test.js
@@ -666,8 +666,8 @@ module('Ember Debug - View', function (hooks) {
       const compSpec = spec.replace('template:components/', 'component:');
       if (
         registry[compSpec] &&
-        !EmberComponentAll.hasComponentTemplate?.(registry[compSpec]))
-      {
+        !EmberComponentAll.hasComponentTemplate?.(registry[compSpec])
+        ){
         EmberComponentAll.setComponentTemplate?.(t.tpl, registry[compSpec]);
       }
     }
