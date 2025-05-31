81ed64501bce6f4f69b950cc423d9d42b4900a86
Patrick Pircher <patricklx@users.noreply.github.com>
2025-05-31 17:48:49 +0200

    fix bounds

--- ember_debug_test_app/tests/ember_debug/view-debug-test.js
+++ ember_debug_test_app/tests/ember_debug/view-debug-test.js
@@ -797,6 +797,7 @@ module('Ember Debug - View', function (hooks) {
       Component(
         {
           name: 'in-element',
+          bounds: 'range',
           args: (actual) => {
             QUnit.assert.ok(actual.positional[0]);
             async function testArgsValue() {
@@ -890,6 +891,7 @@ module('Ember Debug - View', function (hooks) {
       Component(
         {
           name: 'in-element',
+          bounds: 'range',
           args: Args({ names: [], positionals: 1 }),
           template: null,
         },
