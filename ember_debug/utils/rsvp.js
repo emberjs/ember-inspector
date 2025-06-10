5a2e4bae7e7857b6c3f65cc21db029294e178d12
Marine Dunstetter <bluecut.official@gmail.com>
2025-06-10 14:51:48 +0200

    remove old code in rsvp

--- ember_debug/utils/rsvp.js
+++ ember_debug/utils/rsvp.js
@@ -1,21 +1,14 @@
 import Ember, { RSVP as emberRSVP } from './ember';
 
-let module, RSVP;
+let module;
 
 if (emberRSVP) {
   module = emberRSVP;
-  RSVP = module.default;
-
-  // The RSVP module should have named exports for `Promise`, etc,
-  // but some old versions do not and provide `RSVP.Promise`, etc.
-  if (!('Promise' in module)) {
-    module = RSVP;
-  }
 } else {
   // eslint-disable-next-line ember/new-module-imports
-  module = RSVP = Ember.RSVP;
+  module = Ember.RSVP;
 }
 
 export let { Promise, all, resolve } = module;
 
-export default RSVP;
+export default module;
