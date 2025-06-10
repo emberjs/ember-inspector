4d1d3861986ff001f127ced4194096733e5427ca
Marine Dunstetter <bluecut.official@gmail.com>
2025-06-10 15:14:23 +0200

    fix module assignment in runloop

--- ember_debug/utils/ember/runloop.js
+++ ember_debug/utils/ember/runloop.js
@@ -1,18 +1,21 @@
 import Ember, { Runloop as EmberRunloop } from '../ember';
 import * as runloop from './own-runloop';
 
+// it could happen that runloop is available but _backburner is not exported (dead code)
+// then we need to use our own.
 let module = runloop;
 let _backburner = runloop._backburner;
 
 const keys = ['cancel', 'debounce', 'join', 'later', 'scheduleOnce'];
 
 if (EmberRunloop) {
+  module = EmberRunloop;
   _backburner = EmberRunloop._backburner;
 } else {
   // eslint-disable-next-line ember/new-module-imports
-  _backburner = Ember?.run?.backburner || module._backburner;
-  // eslint-disable-next-line ember/new-module-imports
   module = Ember?.run || module;
+  // eslint-disable-next-line ember/new-module-imports
+  _backburner = Ember?.run?.backburner || _backburner;
 }
 
 if (!keys.every((k) => k in module)) {
