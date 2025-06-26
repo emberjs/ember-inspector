0bead41a1e4a69e287c4496e8ba36117c12f054b
Patrick Pircher <patrick.pircher@ibm.com>
2025-06-26 17:13:57 +0200

    show parents in sidebar

--- app/routes/application.js
+++ app/routes/application.js
@@ -12,6 +12,7 @@ export default class ApplicationRoute extends Route {
 
   setupController(controller) {
     controller.set('mixinStack', []);
+    controller.set('router', this);
     let port = this.port;
     port.on('objectInspector:updateObject', this, this.updateObject);
     port.on('objectInspector:updateProperty', this, this.updateProperty);
