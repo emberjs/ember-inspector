a70e6f34fd83d71f4545ffe439109c47c9154e26
Chris Manson <chris@manson.ie>
2025-07-28 12:17:13 +0100

    clean up dist between builds

--- ember_debug/rollup.config.js
+++ ember_debug/rollup.config.js
@@ -2,6 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
 import { globSync } from 'glob';
+import del from 'rollup-plugin-delete';
 
 export default {
   input: [
@@ -20,5 +21,5 @@ export default {
     dir: 'dist',
   },
 
-  plugins: [babel(), nodeResolve(), commonjs()],
+  plugins: [babel(), nodeResolve(), commonjs(), del({ targets: 'dist' })],
 };
