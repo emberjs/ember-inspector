38f060f3fca6ea68aa4ea8b48c384fdf2fc0c3e5
Chris Manson <chris@manson.ie>
2026-05-20 16:02:16 +0100

    move inspector to Vite and have dev mode boot the inspector

--- packages/ember-inspector/app/app.js
+++ packages/ember-inspector/app/app.js
@@ -1,16 +1,20 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'ember-inspector/config/environment';
+import config from "./config/environment";
+
+import setupInspector from "@embroider/legacy-inspector-support/ember-source-4.12";
+import compatModules from "@embroider/virtual/compat-modules";
 
 const version = '{{EMBER_INSPECTOR_VERSION}}';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
+  inspector = setupInspector(this);
 }
 
 config.VERSION = version;
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
