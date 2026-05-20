18c5b34da20821a7e5cda6daf6eae86c726bf1e9
Chris Manson <chris@manson.ie>
2026-05-20 16:13:54 +0100

    half and half

--- packages/ember-inspector/config/environment.js
+++ packages/ember-inspector/config/environment.js
@@ -31,6 +31,8 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.APP.rootElement = '#ember-inspector';
   }
 
   if (environment === 'test') {
