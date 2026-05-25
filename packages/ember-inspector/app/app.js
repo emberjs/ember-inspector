8b4b456fa39a52c99144aa959556d17181c802b0
Chris Manson <chris@manson.ie>
2026-05-25 12:20:09 +0100

    fix showdown

--- packages/ember-inspector/app/app.js
+++ packages/ember-inspector/app/app.js
@@ -7,6 +7,10 @@ import setupInspector from "@embroider/legacy-inspector-support/ember-source-4.1
 import compatModules from "@embroider/virtual/compat-modules";
 
 const version = '{{EMBER_INSPECTOR_VERSION}}';
+// this is a hack to get showdown to be descovered during dependency optimisation
+// this could be fixed properly by converting the app to GJS or by fixing it upstream
+// by either convering ember-cli-showdown to gjs or making it v2 (or both)
+import showdown from 'showdown';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
