3a37c07bd289677020d01d9cbe926c0e6f75eecf
Florian Pichler <fp@ylk.gd>
2026-05-27 16:24:31 +0200

    Move building the extensions to a separate package

--- packages/ember-inspector/ember-cli-build.js
+++ packages/ember-inspector/ember-cli-build.js
@@ -1,21 +1,8 @@
-/* eslint-disable no-undef */
-
 'use strict';
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const VersionChecker = require('ember-cli-version-checker');
-const mergeTrees = require('broccoli-merge-trees');
-const stew = require('broccoli-stew');
-const writeFile = require('broccoli-file-creator');
-const replace = require('broccoli-string-replace');
-const Funnel = require('broccoli-funnel');
-const packageJson = require('./package.json');
-const { mv, rename, rm } = stew;
-const { dirname, join } = require('path');
 
-const {
-  EMBER_VERSIONS_SUPPORTED,
-  PREVIOUS_EMBER_VERSIONS_SUPPORTED,
-} = require('ember-debug/versions');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const replace = require('broccoli-string-replace');
+const packageJson = require('./package.json');
 
 const options = {
   autoImport: {
@@ -27,61 +14,25 @@ const options = {
   svgJar: {
     sourceDirs: ['public/assets/svg'],
   },
+  // Firefox requires non-minified assets for review :(
+  minifyJS: {
+    enabled: false,
+  },
+  minifyCSS: {
+    enabled: false,
+  },
+  vendorFiles: {
+    'jquery.js': null,
+  },
 };
 
-// Firefox requires non-minified assets for review :(
-options.minifyJS = { enabled: false };
-options.minifyCSS = { enabled: false };
-
 module.exports = function (defaults) {
-  let checker = new VersionChecker(defaults);
-  let emberChecker = checker.for('ember-source');
-
-  if (emberChecker.isAbove('3.0.0')) {
-    options.vendorFiles = { 'jquery.js': null };
-  }
-
   if (process.env.EMBER_ENV !== 'test') {
     // https://github.com/ef4/ember-auto-import/issues/540
     options.autoImport.publicAssetURL = 'assets/';
   }
 
-  // When running ember-try on Ember < 3.13, colocation support is
-  // disabled in ember-cli-htmlbars and causes a build error. When
-  // running ember-try, we actually don't care about the "app" side
-  // at all – all we do is run the ember_debug tests (via a --filter
-  // option to ember test in the ember-try config). The only reason
-  // we are even building the app is to get the test harness (qunit
-  // and friends) to work. In the long run, we should split up the
-  // build and not run the app build in ember-try, but in the mean
-  // time, this drops all *.hbs files (but keeping everything else)
-  // to avoid the problem. The app will of course not work correctly
-  // at runtime, but it was never meant to work on old ember versions
-  // in the first place.
-  if (!emberChecker.gte('3.13.0')) {
-    options.trees = {
-      app: new Funnel('app', {
-        exclude: ['**/*.hbs'],
-      }),
-    };
-  }
-
-  let app = new EmberApp(defaults, options);
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  //
-  const env = process.env.EMBER_ENV;
+  const app = new EmberApp(defaults, options);
 
   app.import('vendor/babel-polyfill.js', { prepend: true });
   app.import('node_modules/basiccontext/dist/basicContext.min.css');
@@ -91,204 +42,45 @@ module.exports = function (defaults) {
 
   let tree = app.toTree();
 
-  const emberInspectorVersionPattern = [
-    {
-      match: /{{EMBER_INSPECTOR_VERSION}}/g,
-      replacement: packageJson.version,
-    },
-  ];
-
   tree = replace(tree, {
     files: ['**/*.js'],
-    patterns: emberInspectorVersionPattern,
+    patterns: [
+      {
+        match: /{{EMBER_INSPECTOR_VERSION}}/g,
+        replacement: packageJson.version,
+      },
+    ],
   });
 
-  const minimumVersion = EMBER_VERSIONS_SUPPORTED[0].replace(/\./g, '-');
-  let tabLabel;
-
-  if (process.env.EMBER_INSPECTOR_TAB) {
-    tabLabel = `Ember [${process.env.EMBER_INSPECTOR_TAB}]`;
-  } else if (env === 'development') {
-    tabLabel = `Ember [DEV]`;
-  } else {
-    tabLabel = 'Ember';
-  }
-
-  let replacementPattern = [
-    {
-      match: /{{TAB_LABEL}}/,
-      replacement: tabLabel,
-    },
-    {
-      match: /{{PANE_ROOT}}/g,
-      replacement: `panes-${minimumVersion}`,
-    },
-    {
-      match: /{{PREVIOUS_EMBER_VERSIONS_SUPPORTED}}/g,
-      replacement: JSON.stringify(PREVIOUS_EMBER_VERSIONS_SUPPORTED),
-    },
-    {
-      match: /{{EMBER_VERSIONS_SUPPORTED}}/g,
-      replacement: JSON.stringify(EMBER_VERSIONS_SUPPORTED),
-    },
-  ];
-
-  replacementPattern = replacementPattern.concat(emberInspectorVersionPattern);
-
-  const firefoxBackgroundServiceReplacement = [
-    {
-      match: /"service_worker": "background.js"/g,
-      replacement: '"scripts": ["background.js"]',
-    },
-  ];
-
-  const skeletonWebExtension = replace('skeletons/web-extension', {
-    files: ['*'],
-    patterns: replacementPattern,
-  });
-
-  const skeletonFirefoxWebExtension = replace('skeletons/web-extension', {
-    files: ['*'],
-    patterns: replacementPattern.concat(firefoxBackgroundServiceReplacement),
-  });
-
-  const skeletonBookmarklet = replace('skeletons/bookmarklet', {
-    files: ['*'],
-    patterns: replacementPattern,
-  });
-
-  const webExtensionRoot = `panes-${minimumVersion}`;
-
-  let chrome = mergeTrees([mv(tree, webExtensionRoot), skeletonWebExtension]);
-
-  let firefox = mergeTrees([
-    mv(tree, webExtensionRoot),
-    skeletonFirefoxWebExtension,
-  ]);
-
-  let bookmarklet = mergeTrees([
-    mv(tree, webExtensionRoot),
-    skeletonBookmarklet,
-  ]);
-
-  PREVIOUS_EMBER_VERSIONS_SUPPORTED.forEach(function (version) {
-    version = version.replace(/\./g, '-');
-
-    if (env === 'production') {
-      const prevDist = `dist_prev/${env}`;
-
-      chrome = mergeTrees([
-        mv(`${prevDist}/chrome/panes-${version}`, `panes-${version}`),
-        chrome,
-      ]);
-
-      firefox = mergeTrees([
-        mv(`${prevDist}/firefox/panes-${version}`, `panes-${version}`),
-        firefox,
-      ]);
-
-      bookmarklet = mergeTrees([
-        mv(`${prevDist}/bookmarklet/panes-${version}`, `panes-${version}`),
-        bookmarklet,
-      ]);
-    } else {
-      const file = writeFile(
-        'index.html',
-        'This Ember version is not supported in development environment.',
-      );
-      const emberDebugFile = writeFile('ember_debug.js', 'void(0);');
-
-      chrome = mergeTrees([mv(file, `panes-${version}`), chrome]);
-
-      firefox = mergeTrees([mv(file, `panes-${version}`), firefox]);
-
-      bookmarklet = mergeTrees([
-        mv(file, `panes-${version}`),
-        mv(emberDebugFile, `panes-${version}`),
-        bookmarklet,
-      ]);
-    }
-  });
-
-  const emberDebug = new Funnel(
-    join(dirname(require.resolve('ember-debug/package.json')), 'dist'),
-  );
-
-  // Pass the current dist to the Ember Inspector app.
-  // EMBER DIST
-  const dists = {
-    chrome,
-    firefox,
-    bookmarklet,
-    websocket: tree,
-    basic: tree,
-  };
-
-  const debugRoot = {
-    chrome: webExtensionRoot,
-    firefox: webExtensionRoot,
-    bookmarklet: webExtensionRoot,
-    websocket: '.',
-    basic: '.',
-  };
-
-  Object.keys(dists).forEach(function (key) {
-    dists[key] = replace(dists[key], {
-      files: ['**/*.js'],
-      patterns: [
-        {
-          match: /{{EMBER_DIST}}/g,
-          replacement: key,
-        },
-      ],
-    });
-
-    /**
-     * Add ember-debug
-     * 1. rename dist specific entrypoint to ember_debug.js
-     * 2. remove other unused entrypoints
-     */
-    const debug = mv(
-      rm(
-        rename(emberDebug, (relativePath) => {
-          if (relativePath === `${key}-debug.js`) {
-            return `ember_debug.js`;
-          }
-
-          return relativePath;
-        }),
-        '*-debug.js',
-      ),
-      debugRoot[key],
-    );
-
-    dists[key] = mergeTrees([dists[key], debug]);
-  });
-
-  if (env === 'test') {
-    // `ember test` expects the index.html file to be in the
-    // output directory.
-    // Change base tag for running tests in development env.
-    dists.basic = replace(dists.basic, {
+  if (process.env.EMBER_ENV === 'test') {
+    // `ember test` expects the index.html file to be in the output directory.
+    return replace(tree, {
       files: ['tests/index.html'],
       patterns: [
+        // Change base tag for running tests in development env.
         {
           match: /<base.*\/>/,
           replacement: '',
         },
       ],
     });
-
-    return dists.basic;
   }
 
-  dists.testing = mergeTrees([dists.basic, dists.chrome]);
+  if (
+    process.env.EMBER_ENV === 'test' ||
+    process.env.EMBER_ENV === 'development'
+  ) {
+    tree = replace(tree, {
+      files: ['**/*.js'],
+      patterns: [
+        {
+          match: /{{EMBER_DIST}}/g,
+          replacement:
+            process.env.EMBER_ENV === 'test' ? 'basic' : 'bookmarklet',
+        },
+      ],
+    });
+  }
 
-  return mergeTrees([
-    mv(dists.chrome, 'chrome'),
-    mv(dists.firefox, 'firefox'),
-    mv(dists.bookmarklet, 'bookmarklet'),
-    mv(dists.websocket, 'websocket'),
-    mv(dists.testing, 'testing'),
-  ]);
+  return tree;
 };
